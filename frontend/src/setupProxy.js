938dcb9934766a1b65e218ac0bf4dfa73f170a69
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-11-09 14:49:14 +0200

    fix frontend proxy target

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://localhost:4000',
       changeOrigin: true,
     })
   );
