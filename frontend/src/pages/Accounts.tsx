8b73af59ee91ee418a4c523e22a56e19aad6432a
shamalainen <sebastian@hamse.fi>
2020-11-12 20:09:42 +0200

    Create Hero component

--- frontend/src/pages/Accounts.tsx
+++ frontend/src/pages/Accounts.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Hero from "../components/hero/hero";
 
 const Accounts = (): JSX.Element => {
   return (
     <>
-      <h1>Accounts</h1>
+      <Hero accent="Your" accentColor="blue" label="Accounts">
+        Below you are able to add your various accounts where you have your
+        savings or investments to calculate total amount.
+      </Hero>
     </>
   );
 };
