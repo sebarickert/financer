4ff9fe39d0faecff1629df35ff0e4c15c1150d76
shamalainen <sebastian@hamse.fi>
2020-11-12 17:28:18 +0200

    Install tailwindcss and configure it

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+};
