871ae8be6366381515690786a9bf95222a2d3eab
shamalainen <sebastian@hamse.fi>
2020-11-12 22:08:57 +0200

    Create button component

--- frontend/src/pages/accounts/Accounts.tsx
+++ frontend/src/pages/accounts/Accounts.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../components/button/button";
 import Hero from "../../components/hero/hero";
 
 const Accounts = (): JSX.Element => {
@@ -8,6 +9,11 @@ const Accounts = (): JSX.Element => {
         Below you are able to add your various accounts where you have your
         savings or investments to calculate total amount.
       </Hero>
+      <div className="mt-6">
+        <Button href="/accounts/add" accentColor="blue">
+          Add account
+        </Button>
+      </div>
     </>
   );
 };
