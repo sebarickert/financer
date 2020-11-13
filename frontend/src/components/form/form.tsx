2d9ec489a3819b5ace0cbe8401fe091d21dde613
shamalainen <sebastian@hamse.fi>
2020-11-13 21:55:51 +0200

    Create handleSubmit which adds accounts via form to the database

--- frontend/src/components/form/form.tsx
+++ frontend/src/components/form/form.tsx
@@ -6,11 +6,17 @@ interface IProps {
   children: React.ReactNode;
   formHeading: string;
   submitLabel: string;
+  handleSubmit(event: any): void;
 }
 
-const Form = ({ children, submitLabel, formHeading }: IProps): JSX.Element => {
+const Form = ({
+  children,
+  submitLabel,
+  formHeading,
+  handleSubmit,
+}: IProps): JSX.Element => {
   return (
-    <form>
+    <form onSubmit={handleSubmit} method="post">
       <FormHeader>{formHeading}</FormHeader>
       {children}
       <FormFooter submitLabel={submitLabel} accentColor="blue" />
