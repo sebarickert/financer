660839f2ce0a6fc4ba980fa5329a6c6c76dad1b9
shamalainen <sebastian@hamse.fi>
2020-11-13 21:55:51 +0200

    Create handleSubmit which adds accounts via form to the database

--- frontend/src/pages/accounts/AddAccount.tsx
+++ frontend/src/pages/accounts/AddAccount.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Form from "../../components/form/form";
 import Input from "../../components/input/input";
 import Select, { IOption } from "../../components/select/select";
 
 const AddAccount = (): JSX.Element => {
+  const history = useHistory();
+
   const accountTypes: IOption[] = [
     {
       value: "cash",
@@ -24,8 +27,36 @@ const AddAccount = (): JSX.Element => {
     },
   ];
 
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const { account, amount, type } = event.target;
+    const newAccountData: IAccount = {
+      balance: parseFloat(amount.value),
+      name: account.value,
+      type: type.value,
+    };
+
+    try {
+      const newAccount = await fetch("/api/account", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newAccountData),
+      });
+      history.push("/accounts");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Form submitLabel="Add" formHeading="Add account">
+    <Form
+      submitLabel="Add"
+      formHeading="Add account"
+      handleSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-2">
         <Input
           id="account"
