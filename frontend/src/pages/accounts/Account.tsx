5014b573e7b89e51a275c58e9a704d6ef555043d
shamalainen <sebastian@hamse.fi>
2020-11-15 18:47:03 +0200

    Add delete functionality to account page

--- frontend/src/pages/accounts/Account.tsx
+++ frontend/src/pages/accounts/Account.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Hero from "../../components/hero/hero";
 import Loader from "../../components/loader/loader";
+import ModalConfirm from "../../components/modal/confirm/modal.confirm";
 
 const Account = (): JSX.Element => {
+  const history = useHistory();
   const [account, setAccount] = useState<IAccount | undefined>(undefined);
   const { id } = useParams<{ id: string }>();
 
@@ -15,13 +17,38 @@ const Account = (): JSX.Element => {
     fetchAccount();
   }, [id]);
 
+  const handleDelete = async () => {
+    await fetch(`/api/account/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    history.push("/accounts");
+  };
+
   return typeof account === "undefined" ? (
     <Loader loaderColor="blue" />
   ) : (
-    <Hero accent="Account" accentColor="blue" label={account.name}>
-      Below you are able to edit your accounts information and check your
-      transaction history as well as balance.
-    </Hero>
+    <>
+      <Hero accent="Account" accentColor="blue" label={account.name}>
+        Below you are able to edit your accounts information and check your
+        transaction history as well as balance.
+      </Hero>
+      <div className="mt-6">
+        <ModalConfirm
+          label="Delete account"
+          submitButtonLabel="Delete"
+          onConfirm={handleDelete}
+          modalOpenButtonLabel="Delete account"
+        >
+          Are you sure you want to delete your account? All of your data will be
+          permanently removed. This action cannot be undone.
+        </ModalConfirm>
+      </div>
+    </>
   );
 };
 
