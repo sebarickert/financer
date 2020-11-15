f0e86ea09ef88b6764416c298803cb60e33b4ad9
shamalainen <sebastian@hamse.fi>
2020-11-15 22:26:28 +0200

    Update button group to work responsively

--- frontend/src/components/form/form.footer.tsx
+++ frontend/src/components/form/form.footer.tsx
@@ -13,16 +13,14 @@ const FormFooter = ({
 }: IProps): JSX.Element => {
   return (
     <div className="mt-8 border-t border-gray-200 pt-5">
-      <div className="flex justify-end">
-        <ButtonGroup>
-          <Button accentColor="plain" link="./" size="small">
-            Cancel
-          </Button>
-          <Button accentColor={accentColor} type="submit" size="small">
-            {submitLabel}
-          </Button>
-        </ButtonGroup>
-      </div>
+      <ButtonGroup isReverse>
+        <Button accentColor={accentColor} type="submit" size="small">
+          {submitLabel}
+        </Button>
+        <Button accentColor="plain" link="./" size="small">
+          Cancel
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
