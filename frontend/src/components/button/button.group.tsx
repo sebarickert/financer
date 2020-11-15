8fb7202c2063d5c1860519d19d8008b9b52fca4c
shamalainen <sebastian@hamse.fi>
2020-11-15 22:26:28 +0200

    Update button group to work responsively

--- frontend/src/components/button/button.group.tsx
+++ frontend/src/components/button/button.group.tsx
@@ -3,21 +3,32 @@ import React from "react";
 interface IProps {
   readonly children: React.ReactNode[];
   label?: string;
+  isReverse?: boolean;
+  className?: string;
 }
 
-const ButtonGroup = ({ children, label }: IProps): JSX.Element => {
+const ButtonGroup = ({
+  children,
+  label,
+  className = "",
+  isReverse,
+}: IProps): JSX.Element => {
+  const horizontalMargin = isReverse ? "sm:mr-3" : "sm:ml-3";
+
   return (
-    <div>
+    <div className={className}>
       {label && (
         <h2 className="text-2xl leading-9 font-bold tracking-tight text-gray-900 mb-4">
           {label}
         </h2>
       )}
-      <div className="sm:flex">
+      <div className={`sm:flex  ${isReverse ? "sm:flex-row-reverse" : ""}`}>
         {/* eslint-disable react/no-array-index-key */}
         {children.map((child, index) => (
           <span
-            className={`${index > 0 ? "ml-3" : ""}`}
+            className={`block ${
+              index > 0 ? `mt-3 sm:mt-0 ${horizontalMargin}` : ""
+            }`}
             key={`button-${index}`}
           >
             {child}
--- frontend/src/components/modal/confirm/modal.confirm.actions.tsx
+++ frontend/src/components/modal/confirm/modal.confirm.actions.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "../../button/button";
+import ButtonGroup from "../../button/button.group";
 
 interface IProps {
   onCancel(): void;
@@ -12,26 +14,14 @@ const ModalConfirmActions = ({
   submitButtonLabel,
 }: IProps): JSX.Element => {
   return (
-    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-      <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-        <button
-          type="button"
-          className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-          onClick={onConfirm}
-        >
-          {submitButtonLabel}
-        </button>
-      </span>
-      <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-        <button
-          type="button"
-          className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-      </span>
-    </div>
+    <ButtonGroup className="bg-gray-50 px-4 py-3 sm:px-6" isReverse>
+      <Button onClick={onConfirm} accentColor="red" size="small">
+        {submitButtonLabel}
+      </Button>
+      <Button onClick={onCancel} accentColor="plain" size="small">
+        Cancel
+      </Button>
+    </ButtonGroup>
   );
 };
 
