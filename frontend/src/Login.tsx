1fb5a5fb08f77f42528956b0061766c3f958ddd7
shamalainen <sebastian@hamse.fi>
2020-11-15 22:34:59 +0200

    Add WIP navigation

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "./components/button/button";
-import Layout from "./components/layout/layout";
+import Container from "./components/container/container";
 
 const Login = (): JSX.Element => {
   return (
-    <Layout className="mt-12 flex justify-center">
+    <Container className="mt-12 flex justify-center">
       <Button link="/api/auth/github">Login with Github</Button>
-    </Layout>
+    </Container>
   );
 };
 
