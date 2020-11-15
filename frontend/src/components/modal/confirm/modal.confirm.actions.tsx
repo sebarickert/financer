bccf15e35585d5ef1a71457ab2f16877eb9a9905
shamalainen <sebastian@hamse.fi>
2020-11-15 18:45:06 +0200

    Created modal confirm functionality

--- frontend/src/components/modal/confirm/modal.confirm.actions.tsx
+++ frontend/src/components/modal/confirm/modal.confirm.actions.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 interface IProps {
+  onCancel(): void;
+  onConfirm(): void;
   submitButtonLabel: string;
 }
 
-const ModalConfirmActions = ({ submitButtonLabel }: IProps): JSX.Element => {
+const ModalConfirmActions = ({
+  onCancel,
+  onConfirm,
+  submitButtonLabel,
+}: IProps): JSX.Element => {
   return (
     <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
       <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
         <button
           type="button"
           className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+          onClick={onConfirm}
         >
           {submitButtonLabel}
         </button>
@@ -19,6 +26,7 @@ const ModalConfirmActions = ({ submitButtonLabel }: IProps): JSX.Element => {
         <button
           type="button"
           className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+          onClick={onCancel}
         >
           Cancel
         </button>
