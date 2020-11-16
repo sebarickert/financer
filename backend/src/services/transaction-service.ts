f65d9fa4004a95db5d78b96e1f03c978a14ee96a
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-11-16 20:14:17 +0200

    Init transaction endpoints

---
+++ backend/src/services/transaction-service.ts
@@ -0,0 +1,16 @@
+import transactionModel, {
+  ITransactionModel,
+} from "../models/transaction-model";
+
+export const findIncomeTransactionsByUser = async (
+  userId: string
+): Promise<ITransactionModel[] | null> =>
+  transactionModel.find({ user: userId, toAccount: { $ne: undefined } });
+
+export const findAccountsById = async (
+  id: string
+): Promise<ITransactionModel | null> => transactionModel.findById(id);
+
+export const createTransaction = async (
+  newAccount: ITransactionModel
+): Promise<ITransactionModel | null> => transactionModel.create(newAccount);
