6d66aa6dd022b82a9ecc6b1cb6b42032f2df08eb
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-11-16 10:47:45 +0200

    Move DB host setting to env

--- backend/src/config/load-env.ts
+++ backend/src/config/load-env.ts
@@ -1,9 +1,15 @@
 import { config, parse } from "dotenv";
 import { readFileSync } from "fs";
+import fileExists from "../utils/fileExists";
 
 config();
-const localConfig = parse(readFileSync(".env.local"));
 
-Object.entries(localConfig).forEach(([key, value]) => {
-  process.env[key] = value;
-});
+const localConfigExists = fileExists(".env.local")
+
+if(localConfigExists) {
+  const localConfig = parse(readFileSync(".env.local"));
+
+  Object.entries(localConfig).forEach(([key, value]) => {
+    process.env[key] = value;
+  });  
+}
