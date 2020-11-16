aabfdc3f925befb9771f0f16c5013fd8cc20ce0b
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-11-16 19:45:19 +0200

    Add transaction data model and type interfaces

---
+++ backend/src/models/transaction-model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { ACCOUNT_MODEL_NAME } from "./account-model";
+import { USER_MODEL_NAME } from "./user-model";
+
+export const TRANSACTION_MODEL_NAME = "transaction";
+export interface ITransactionModel extends Document, ITransaction {
+  _id: any;
+}
+
+const transactionSchema = new Schema({
+  fromAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ACCOUNT_MODEL_NAME,
+  },
+  toAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ACCOUNT_MODEL_NAME,
+  },
+  fromAccountBalance: Number,
+  toAccountBalance: Number,
+  amount: Number,
+  description: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: USER_MODEL_NAME,
+  },
+});
+
+transactionSchema
+  .path("fromAccountBalance")
+  .get((num: number) => parseFloat((num / 100).toFixed(2)));
+transactionSchema.path("fromAccountBalance").set((num: number) => num * 100);
+
+transactionSchema
+  .path("toAccountBalance")
+  .get((num: number) => parseFloat((num / 100).toFixed(2)));
+transactionSchema.path("toAccountBalance").set((num: number) => num * 100);
+
+export default mongoose.model<ITransactionModel>(TRANSACTION_MODEL_NAME, transactionSchema);
