ed0496a8b602927209cf487188281bd7e4cd7361
shamalainen <sebastian@hamse.fi>
2020-11-16 20:25:20 +0200

    Update stats header to not be compulsory

--- frontend/src/components/stats/stats.header.tsx
+++ frontend/src/components/stats/stats.header.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const StatsHeader = ({ children }: IProps): JSX.Element => {
   return (
-    <h2 className="text-2xl leading-9 font-bold tracking-tight text-gray-900 mb-2 sm:mb-0 sm:leading-none">
+    <h2 className="mb-4 text-2xl leading-9 font-bold tracking-tight text-gray-900 sm:leading-none">
       {children}
     </h2>
   );
