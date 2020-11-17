2789f61d72db7b9368086f71f325ef8b5724a231
shamalainen <sebastian@hamse.fi>
2020-11-17 20:09:34 +0200

    Update expense detail page

--- frontend/src/pages/expenses/Expense.tsx
+++ frontend/src/pages/expenses/Expense.tsx
@@ -8,75 +8,76 @@ import ModalConfirm from "../../components/modal/confirm/modal.confirm";
 import Stats from "../../components/stats/stats";
 import StatsItem from "../../components/stats/stats.item";
 import formatCurrency from "../../utils/formatCurrency";
+import formatDate from "../../utils/formatDate";
 
 interface IProps {
   handleDelete(): void;
 }
 
-const AccountDeleteModal = ({ handleDelete }: IProps) => (
+const ExpenseDeleteModal = ({ handleDelete }: IProps) => (
   <ModalConfirm
-    label="Delete account"
+    label="Delete expense"
     submitButtonLabel="Delete"
     onConfirm={handleDelete}
-    modalOpenButtonLabel="Delete account"
+    modalOpenButtonLabel="Delete expense"
     accentColor="red"
   >
-    Are you sure you want to delete your account? All of your data will be
+    Are you sure you want to delete your expense? All of your data will be
     permanently removed. This action cannot be undone.
   </ModalConfirm>
 );
 
 const Expense = (): JSX.Element => {
-  // const history = useHistory();
-  // const [account, setAccount] = useState<IAccount | undefined>(undefined);
-  // const { id } = useParams<{ id: string }>();
+  const history = useHistory();
+  const [expense, setExpense] = useState<IExpense | undefined>(undefined);
+  const { id } = useParams<{ id: string }>();
 
-  // useEffect(() => {
-  //   const fetchAccount = async () => {
-  //     const rawAccount = await fetch(`/api/account/${id}`);
-  //     setAccount((await rawAccount.json()).payload);
-  //   };
-  //   fetchAccount();
-  // }, [id]);
+  useEffect(() => {
+    const fetchExpense = async () => {
+      const rawExpense = await fetch(`/api/expense/${id}`);
+      setExpense((await rawExpense.json()).payload);
+    };
+    fetchExpense();
+  }, [id]);
 
-  // const handleDelete = async () => {
-  //   await fetch(`/api/account/${id}`, {
-  //     method: "DELETE",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
+  const handleDelete = async () => {
+    await fetch(`/api/expense/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-  //   history.push("/accounts");
-  // };
+    history.push("/expenses");
+  };
 
-  // return account ? (
-  //   <Loader loaderColor="red" />
-  // ) : (
-  //   <>
-  //     <Hero accent="Account" accentColor="blue" label={account.name}>
-  //       Below you are able to edit your accounts information and check your
-  //       transaction history as well as balance.
-  //     </Hero>
-  //     <div className="mt-6">
-  //       <ButtonGroup>
-  //         <Button accentColor="blue" link={`/accounts/${id}/edit`}>
-  //           Edit account
-  //         </Button>
-  //         <AccountDeleteModal handleDelete={handleDelete} />
-  //       </ButtonGroup>
-  //     </div>
-  //     <Stats className="mt-12" label="Overview">
-  //       <StatsItem statLabel="Balance">
-  //         {formatCurrency(account.balance)}
-  //       </StatsItem>
-  //       <StatsItem statLabel="Type">{account.type}</StatsItem>
-  //     </Stats>
-  //   </>
-  // );
-
-  return <h1>Expense</h1>;
+  return typeof expense === "undefined" ? (
+    <Loader loaderColor="red" />
+  ) : (
+    <>
+      <Hero accent="Income" accentColor="red" label={expense.description}>
+        Below you are able to edit your accounts information and check your
+        transaction history as well as balance.
+      </Hero>
+      <div className="mt-6">
+        <ButtonGroup>
+          <Button accentColor="blue" link={`/incomes/${id}/edit`}>
+            Edit expense
+          </Button>
+          <ExpenseDeleteModal handleDelete={handleDelete} />
+        </ButtonGroup>
+      </div>
+      <Stats className="mt-12" label="Overview">
+        <StatsItem statLabel="Amount">
+          {formatCurrency(expense.amount)}
+        </StatsItem>
+        <StatsItem statLabel="Date">
+          {formatDate(new Date(expense.date))}
+        </StatsItem>
+      </Stats>
+    </>
+  );
 };
 
 export default Expense;
