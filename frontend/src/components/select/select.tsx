68b16b83bb1a9473323557a97401d94d98c4cecb
shamalainen <sebastian@hamse.fi>
2020-11-18 21:32:48 +0200

    Update tailwind version

--- frontend/src/components/select/select.tsx
+++ frontend/src/components/select/select.tsx
@@ -33,7 +33,7 @@ const Select = ({
           id={id}
           name={id}
           defaultValue={defaultValue}
-          className="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           required={isRequired}
           aria-describedby={help && `${id}-description`}
         >
