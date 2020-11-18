459f66cd31f85f4b41248d397b9b7f79188831e4
shamalainen <sebastian@hamse.fi>
2020-11-18 17:07:59 +0200

    Add new account type

--- shared-types/account.d.ts
+++ shared-types/account.d.ts
@@ -1,7 +1,7 @@
 interface IAccount {
   _id?: any;
   name: string;
-  type: "cash" | "savings" | "investment" | "credit";
+  type: "cash" | "savings" | "investment" | "credit" | "loan";
   balance: number;
   owner?: string;
 }
