1f4e622a7eaadc4dd9c9d557a99decd9febd00cb
shamalainen <sebastian@hamse.fi>
2020-11-24 18:45:23 +0200

    Update logo to be link to dashboard

--- frontend/src/components/navigation/navigation.logo.tsx
+++ frontend/src/components/navigation/navigation.logo.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 const NavigationLogo = (): JSX.Element => {
   return (
     <div className="flex-shrink-0 flex items-center">
-      <Logo className="block h-10 w-auto" />
+      <Link to="/">
+        <Logo className="block h-10 w-auto" />
+      </Link>
     </div>
   );
 };
