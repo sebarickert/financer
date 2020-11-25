a12b6f2ceee2c62f803856bd8313054eb0dac85a
shamalainen <sebastian@hamse.fi>
2020-11-25 21:09:36 +0200

    Create login modal component

---
+++ frontend/src/components/login/login.actions.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../button/button";
+
+interface IProps {
+  submitButtonLabel: string;
+}
+
+const LoginActions = ({ submitButtonLabel }: IProps): JSX.Element => {
+  return (
+    <div className="bg-gray-50 px-4 py-3 sm:px-6 flex justify-end">
+      <Button link="/api/auth/github">{submitButtonLabel}</Button>
+    </div>
+  );
+};
+
+export default LoginActions;
