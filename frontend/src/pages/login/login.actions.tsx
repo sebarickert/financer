b33c08bf280a82f441b12dd42c431072f75b72c4
shamalainen <sebastian@hamse.fi>
2020-11-26 20:55:09 +0200

    Fix router issue

--- frontend/src/pages/login/login.actions.tsx
+++ frontend/src/pages/login/login.actions.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../button/button";
+import Button from "../../components/button/button";
 
 interface IProps {
   submitButtonLabel: string;
--- frontend/src/pages/login/login.footer.tsx
+++ frontend/src/pages/login/login.footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../button/button";
 
 interface IProps {
   className: string;
