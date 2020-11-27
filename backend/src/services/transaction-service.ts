8ab6381706ebe2d982f6a743df9179cdff52b15c
shamalainen <sebastian@hamse.fi>
2020-11-27 23:57:14 +0200

    Remove transactions from income/expenses listing

--- backend/src/services/transaction-service.ts
+++ backend/src/services/transaction-service.ts
@@ -5,12 +5,20 @@ import transactionModel, {
 export const findIncomeTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({ user: userId, toAccount: { $ne: undefined } });
+  transactionModel.find({
+    user: userId,
+    toAccount: { $ne: undefined },
+    fromAccount: { $eq: undefined },
+  });
 
 export const findExpenseTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({ user: userId, fromAccount: { $ne: undefined } });
+  transactionModel.find({
+    user: userId,
+    fromAccount: { $ne: undefined },
+    toAccount: { $eq: undefined },
+  });
 
 export const findTransactionById = async (
   id: string
