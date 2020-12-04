2bbde9bae0a4512411b76717562a8fd2cdec66b0
shamalainen <sebastian@hamse.fi>
2020-12-04 22:45:52 +0200

    Update stacked list header styles

--- frontend/src/components/stacked-list/stacked-list.header.tsx
+++ frontend/src/components/stacked-list/stacked-list.header.tsx
@@ -12,18 +12,17 @@ interface IProps {
 
 const StackedListHeader = ({ label, addiotinalLabel }: IProps): JSX.Element => {
   return (
-    <div className="sm:flex sm:items-end sm:justify-between">
-      <h2 className="text-2xl leading-9 font-bold tracking-tight text-gray-900 mb-2 sm:mb-0 sm:leading-none">
+    <div className="flex items-center justify-between">
+      <h2 className="text-2xl font-bold tracking-tight leading-none truncate">
         {label}
       </h2>
       {addiotinalLabel && (
-        <h3 className="text-xl tracking-tight text-gray-900 leading-none">
+        <h3 className="text-xl tracking-tight text-gray-900 leading-none flex-shrink-0 text-right ml-4 inline-flex items-center">
           {addiotinalLabel.accentLabel && (
             <>
-              <span className="text-sm text-gray-500 mb-2 inline-block">
+              <span className="text-sm  mr-2 inline-block leading-none">
                 {addiotinalLabel.accentLabel}
               </span>
-              <br />
             </>
           )}
           {addiotinalLabel.label}
