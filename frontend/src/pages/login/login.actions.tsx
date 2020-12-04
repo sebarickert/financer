6d16b9387dd2f6d98778950c8358fe759965078f
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-12-04 16:09:37 +0200

    Add Auth0 authentication endpoint

--- frontend/src/pages/login/login.actions.tsx
+++ frontend/src/pages/login/login.actions.tsx
@@ -3,12 +3,13 @@ import Button from "../../components/button/button";
 
 interface IProps {
   submitButtonLabel: string;
+  loginUrl: string;
 }
 
-const LoginActions = ({ submitButtonLabel }: IProps): JSX.Element => {
+const LoginActions = ({ submitButtonLabel, loginUrl }: IProps): JSX.Element => {
   return (
     <div className="bg-gray-50 px-4 py-3 sm:px-6 flex justify-end">
-      <Button link="/api/auth/github">{submitButtonLabel}</Button>
+      <Button link={loginUrl}>{submitButtonLabel}</Button>
     </div>
   );
 };
--- shared-types/user.d.ts
+++ shared-types/user.d.ts
@@ -2,6 +2,7 @@ interface IUser {
   _id: any;
   name: any;
   nickname: string;
-  githubId: string;
+  githubId?: string;
+  auth0Id?: string;
   profileImageUrl: string;
 }
