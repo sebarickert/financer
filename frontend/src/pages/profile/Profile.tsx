e7ac3426f11cb39d681f44b665baf08fa079d5ec
Teemu Sillantaus <github@silte.fi>
2020-12-09 20:59:51 +0200

    Add support to upload mydata as json file

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,40 +1,103 @@
-import React from "react";
+/* eslint-disable no-alert */
+/* eslint-disable consistent-return */
+import React, { useEffect, useState } from "react";
 import Button from "../../components/button/button";
+import DescriptionList from "../../components/description-list/description-list";
+import DescriptionListItem from "../../components/description-list/description-list.item";
+import capitalize from "../../utils/capitalize";
+import formatCurrency from "../../utils/formatCurrency";
+
+interface IOveridata {
+  accounts: IAccount[];
+  transactions: ITransaction[];
+  user: IUser;
+}
 
 const Profile = (): JSX.Element => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
-    const { value } = event.target.userData;
+  const [uploadedUserData, setUploadedUserData] = useState<IOveridata | null>(
+    null
+  );
+  const [overrideTranactionCount, setOverrideTranactionCount] = useState<
+    number | null
+  >(null);
+  const [overrideAccountCount, setOverrideAccountCount] = useState<
+    number | null
+  >(null);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  useEffect(() => {
+    if (!uploadedUserData) {
+      setOverrideTranactionCount(null);
+      setOverrideAccountCount(null);
+      return;
+    }
+
+    setOverrideTranactionCount(uploadedUserData.transactions.length);
+    setOverrideAccountCount(uploadedUserData.accounts.length);
+  }, [uploadedUserData]);
+
+  const handleOverrideData = async () => {
     const rawOverride = await fetch("/api/profile/my-data", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: value,
+      body: JSON.stringify(uploadedUserData),
     });
     const override: IApiResponse<string> = await rawOverride.json();
     if (override.status < 300) {
-      // eslint-disable-next-line no-alert
       alert(`Successfully overridden: ${override?.payload}`);
     } else {
-      // eslint-disable-next-line no-alert
       alert(`Failed override: ${override.errors?.join(", ")}`);
     }
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleFileChange = (changeEvent: any) => {
+    const { files } = changeEvent?.target;
+    if (files.length <= 0) {
+      return alert("File not found");
+    }
+
+    const fr = new FileReader();
+    fr.onload = (readerEvent) => {
+      if (
+        readerEvent?.target?.result &&
+        typeof readerEvent?.target?.result === "string"
+      ) {
+        const result = JSON.parse(readerEvent.target.result);
+        setUploadedUserData(result);
+      } else {
+        alert("Failed to parse JSON file");
+      }
+    };
+    fr.readAsText(files.item(0));
+  };
+
   return (
     <>
-      <Button link="/api/profile/my-data">Download my data</Button>
-      <form className="block mt-12" onSubmit={handleSubmit}>
-        <textarea name="userData" id="userData" className="w-full" />
-        <Button type="submit" accentColor="red" className="mt-2">
-          Override my data
-        </Button>
-      </form>
+      <Button link="/api/profile/my-data" className="block">
+        Download my data
+      </Button>
+      <input
+        className="block mt-16"
+        type="file"
+        id="selectFiles"
+        onChange={handleFileChange}
+        accept="application/json"
+      />
+      <DescriptionList label="Override data details" className="mt-6">
+        <DescriptionListItem label="Account count">
+          {overrideAccountCount ? `${overrideAccountCount}` : "-"}
+        </DescriptionListItem>
+        <DescriptionListItem label="Transaction count">
+          {overrideTranactionCount ? `${overrideTranactionCount}` : "-"}
+        </DescriptionListItem>
+      </DescriptionList>
+
+      <Button onClick={handleOverrideData} accentColor="red" className="mt-6">
+        Override my data
+      </Button>
     </>
   );
 };
