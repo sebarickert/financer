e3b8698379ac20cfa99e760576e919f71185bf65
shamalainen <sebastian@hamse.fi>
2020-12-10 21:40:09 +0200

    Create profile navigation component

--- frontend/src/pages/profile/ProfileRouter.tsx
+++ frontend/src/pages/profile/ProfileRouter.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Container from "../../components/container/container";
 import Profile from "./Profile";
+import ProfileNavigation from "./ProfileNavigation";
 
 const ProfileRouter = (): JSX.Element => {
   return (
-    <Switch>
-      <Route exact path="/profile">
-        <Profile />
-      </Route>
-    </Switch>
+    <Container
+      className="mt-6 sm:mt-12"
+      sidebarComponent={<ProfileNavigation />}
+    >
+      <Switch>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Container>
   );
 };
 
