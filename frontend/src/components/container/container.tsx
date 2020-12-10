cfa7e1ec802a2816cff6402d5d98877e49ffd352
shamalainen <sebastian@hamse.fi>
2020-12-10 21:17:02 +0200

    Create sidebar functionality for container

--- frontend/src/components/container/container.tsx
+++ frontend/src/components/container/container.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import Sidebar from "../sidebar/sidebar";
 
 interface IProps {
   className?: string;
   children: React.ReactNode;
+  sidebarComponent?: React.ReactNode;
 }
 
-const Container = ({ className = "", children }: IProps): JSX.Element => {
+const Container = ({
+  className = "",
+  children,
+  sidebarComponent,
+}: IProps): JSX.Element => {
   return (
-    <div className={`mx-auto px-4 max-w-screen-xl ${className}`}>
-      {children}
+    <div
+      className={`mx-auto px-4 max-w-screen-xl ${className} ${
+        sidebarComponent ? "lg:flex" : ""
+      }`}
+    >
+      {sidebarComponent && <Sidebar>{sidebarComponent}</Sidebar>}
+      {sidebarComponent ? (
+        <div className="lg:flex-1">{children}</div>
+      ) : (
+        children
+      )}
     </div>
   );
 };
