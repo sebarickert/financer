bb7bd748fbd36dbab2437a405258a1b6c86d3831
shamalainen <sebastian@hamse.fi>
2021-04-23 20:19:28 +0300

    Make chart look good

--- frontend/src/pages/accounts/Account.tsx
+++ frontend/src/pages/accounts/Account.tsx
@@ -3,11 +3,11 @@ import { useHistory, useParams } from "react-router-dom";
 import {
   Area,
   AreaChart,
+  Brush,
   CartesianGrid,
-  Line,
-  LineChart,
   ResponsiveContainer,
   Tooltip,
+  TooltipProps,
   XAxis,
   YAxis,
 } from "recharts";
@@ -22,6 +22,8 @@ import ModalConfirm from "../../components/modal/confirm/modal.confirm";
 import SEO from "../../components/seo/seo";
 import StackedList from "../../components/stacked-list/stacked-list";
 import { ICustomStackedListRowProps } from "../../components/stacked-list/stacked-list.row";
+import monthNames, { MONTH_IN_MS } from "../../constants/months";
+import addLeadingZero from "../../utils/addLeadingZero";
 import capitalize from "../../utils/capitalize";
 import formatCurrency from "../../utils/formatCurrency";
 import { formatDate } from "../../utils/formatDate";
@@ -45,19 +47,71 @@ interface ISimpleLineChartProps {
   data: IChartData[];
 }
 
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+}: TooltipProps): JSX.Element => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="py-2 px-4 bg-blue-600 shadow-lg rounded-md">
+        <p className="text-white">
+          Balance {formatCurrency(payload[0].value as number)}
+        </p>
+        <p className="text-white">{label}</p>
+      </div>
+    );
+  }
+
+  return <div />;
+};
+
 const SimpleLineChart = ({ data }: ISimpleLineChartProps): JSX.Element => {
+  const monthAgoDate = new Date().getTime() - MONTH_IN_MS;
+  const monthAgoIndex = data.indexOf(
+    data.find((tick) => tick.date.getTime() > monthAgoDate) || data[0]
+  );
+
   return (
     <div style={{ width: "100%", height: "33vh", minHeight: "450px" }}>
       <ResponsiveContainer>
         <AreaChart data={data}>
-          <Tooltip />
+          <defs>
+            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#1c64f2" stopOpacity={0.4} />
+              <stop offset="75%" stopColor="#1c64f2" stopOpacity={0.05} />
+            </linearGradient>
+          </defs>
+          <Tooltip content={CustomTooltip} />
           <YAxis
+            dataKey="balance"
+            axisLine={false}
+            tickLine={false}
+            tickCount={6}
             domain={["dataMin", "dataMax"]}
-            tickFormatter={formatCurrency}
+            tickFormatter={(number) => formatCurrency(number)}
+            width={105}
           />
-          <XAxis dataKey="dateStr" minTickGap={20} />
-          <Area dataKey="balance" strokeWidth={3} dot={false} />
-          <CartesianGrid vertical={false} stroke="#CCCCCC" />
+          <XAxis
+            dataKey="dateStr"
+            axisLine={false}
+            tickLine={false}
+            tickMargin={15}
+            height={50}
+          />
+          <Area
+            dataKey="balance"
+            stroke="#1c64f2"
+            fill="url(#color)"
+            strokeWidth={2}
+          />
+          <Brush
+            dataKey="dateStr"
+            height={30}
+            stroke="#1c64f2"
+            startIndex={monthAgoIndex}
+          />
+          <CartesianGrid vertical={false} opacity={0.5} />
         </AreaChart>
       </ResponsiveContainer>
     </div>
