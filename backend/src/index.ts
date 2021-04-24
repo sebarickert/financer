1a91b8d9c5c1a3a7a48d59e8cd67a766d0f06ac2
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2021-04-24 17:48:12 +0300

    Add handling for process kill signals

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Socket } from "net";
 
 import "./config/load-env";
 import "./config/passport-setup";
@@ -6,6 +7,8 @@ import { MONGODB_URI } from "./config/keys";
 import app from "./server";
 
 const port = 4000; // default port to listen
+
+let connections: Socket[] = [];
 
 // connect to mongodb
 mongoose.connect(
@@ -24,7 +27,38 @@ mongoose.connect(
   }
 );
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`server started at http://localhost:${port}`);
 });
+
+server.on("connection", (connection) => {
+  connections.push(connection);
+  connection.on("close", () => {
+    connections = connections.filter((curr) => curr !== connection);
+  });
+});
+
+const shutDown = () => {
+  // eslint-disable-next-line no-console
+  console.log("Received kill signal, shutting down gracefully");
+  server.close(() => {
+    // eslint-disable-next-line no-console
+    console.log("Closed out remaining connections");
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    // eslint-disable-next-line no-console
+    console.error(
+      "Could not close connections in time, forcefully shutting down"
+    );
+    process.exit(1);
+  }, 10000);
+
+  connections.forEach((curr) => curr.end());
+  setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
+};
+
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
