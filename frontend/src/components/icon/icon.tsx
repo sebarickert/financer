99d9179d699f3eef0f3c75abd08bb0ab2393191d
shamalainen <sebastian@hamse.fi>
2021-04-28 16:52:17 +0300

    Update button looks

--- frontend/src/components/icon/icon.tsx
+++ frontend/src/components/icon/icon.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import IconSwitchHorizontal from "./icon.switchHorizontal";
 import IconPlusCircle from "./icon.plusCircle";
+import IconMinusCircle from "./icon.minusCircle";
 
+export type IconName = "switch-horizontal" | "plus-circle" | "minus-circle";
 interface IconProps {
-  type: "switch-horizontal" | "plus-circle";
+  type: IconName;
 }
 
 const Icon = ({ type }: IconProps): JSX.Element => {
+  const defaultIconClasses = "h-6 w-6";
+
   switch (type) {
     case "switch-horizontal":
-      return <IconSwitchHorizontal />;
+      return <IconSwitchHorizontal className={defaultIconClasses} />;
 
     case "plus-circle":
-      return <IconPlusCircle />;
+      return <IconPlusCircle className={defaultIconClasses} />;
+
+    case "minus-circle":
+      return <IconMinusCircle className={defaultIconClasses} />;
 
     default:
       break;
