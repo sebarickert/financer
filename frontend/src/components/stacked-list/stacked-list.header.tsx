a05cef9a3f5561116c8d2b373d8a3989413cc233
shamalainen <sebastian@hamse.fi>
2021-04-28 16:27:23 +0300

    Update stacked list look

--- frontend/src/components/stacked-list/stacked-list.header.tsx
+++ frontend/src/components/stacked-list/stacked-list.header.tsx
@@ -12,12 +12,12 @@ interface IProps {
 
 const StackedListHeader = ({ label, addiotinalLabel }: IProps): JSX.Element => {
   return (
-    <div className="flex flex-col shadow-lg p-6 bg-gray-800 rounded sticky top-4 lg:bg-white lg:shadow lg:flex-shrink-0 lg:w-80 lg:mr-6 space-y-6 mb-4">
-      <h2 className="text-2xl font-bold tracking-tight leading-none truncate text-white lg:text-gray-900">
+    <div className="flex flex-col shadow-md lg:shadow-none p-6 bg-gray-800 sticky top-4 lg:flex-shrink-0 lg:w-80 lg:mr-6 space-y-6 mb-4">
+      <h2 className="text-xl font-bold tracking-tight leading-none truncate text-white">
         {label}
       </h2>
       {addiotinalLabel && (
-        <h3 className="text-lg tracking-tigh leading-none flex-shrink-0 inline-flex justify-between text-white lg:text-gray-900">
+        <h3 className="text-lg tracking-tigh leading-none flex-shrink-0 inline-flex justify-between text-white">
           {addiotinalLabel.accentLabel && (
             <>
               <span className="truncate">{addiotinalLabel.accentLabel}</span>
