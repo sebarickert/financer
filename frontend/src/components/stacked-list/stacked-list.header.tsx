6dffa3a4f23afbbe0f82f9c53353c3a1a9bf62b7
shamalainen <sebastian@hamse.fi>
2021-04-30 16:34:52 +0300

    Update stacked-list color

--- frontend/src/components/stacked-list/stacked-list.header.tsx
+++ frontend/src/components/stacked-list/stacked-list.header.tsx
@@ -12,7 +12,7 @@ interface IProps {
 
 const StackedListHeader = ({ label, addiotinalLabel }: IProps): JSX.Element => {
   return (
-    <div className="flex flex-col shadow-md lg:shadow-none p-6 bg-gray-800 sticky top-4 lg:flex-shrink-0 lg:w-80 lg:mr-6 space-y-6 mb-4">
+    <div className="flex flex-col shadow-md lg:shadow-none p-6 bg-gray-900 sticky top-0 lg:top-4 lg:flex-shrink-0 lg:w-80 lg:mr-4 space-y-6 -mx-4 lg:mx-0">
       <h2 className="text-xl font-bold tracking-tight leading-none truncate text-white">
         {label}
       </h2>
