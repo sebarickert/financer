59c650275e4bac5c57cf9c256c6baa78d8cf13f0
shamalainen <sebastian@hamse.fi>
2021-09-02 19:40:20 +0300

    Fix build

--- frontend/src/services/AuthenticationService.ts
+++ frontend/src/services/AuthenticationService.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getAuthenticationStatus = async (): Promise<IAuthenticationStatus> => {
-  const authenticationStatus = await fetch("/auth/api/status");
-  return authenticationStatus.json();
-};
+export const getAuthenticationStatus =
+  async (): Promise<IAuthenticationStatus> => {
+    const authenticationStatus = await fetch("/auth/api/status");
+    return authenticationStatus.json();
+  };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,5 +18,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/ui"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms")],
 }
