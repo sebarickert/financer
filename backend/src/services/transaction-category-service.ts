d7dbf12156692eaa67a89b8d8a76c3927912dcd3
shamalainen <sebastian@hamse.fi>
2021-09-13 20:41:41 +0300

    Add deleting logic to transaction categories

--- backend/src/services/transaction-category-service.ts
+++ backend/src/services/transaction-category-service.ts
@@ -1,3 +1,4 @@
+import { truncate } from "fs";
 import transactionCategoryModel, {
   ITransactionCategoryModel,
 } from "../models/transaction-category-model";
@@ -5,14 +6,32 @@ import transactionCategoryModel, {
 export const findTransactionCategoriesByUser = async (
   userId: string
 ): Promise<ITransactionCategoryModel[] | null> =>
-  transactionCategoryModel.find({ owner: userId });
+  transactionCategoryModel.find({
+    owner: userId,
+    deleted: { $ne: true },
+  });
 
 export const findTransactionCategoryById = async (
   id: string
 ): Promise<ITransactionCategoryModel | null> =>
-  transactionCategoryModel.findById(id);
+  transactionCategoryModel.findOne({
+    _id: id,
+    deleted: { $ne: true },
+  });
 
 export const createTransactionCategory = async (
   newTransactionCategory: ITransactionCategoryModel
 ): Promise<ITransactionCategoryModel | null> =>
   transactionCategoryModel.create(newTransactionCategory);
+
+export const markTransactionCategoryAsDeleted = async (
+  id: string
+): Promise<ITransactionCategoryModel | null> => {
+  const transactionCategory = await findTransactionCategoryById(id);
+
+  if (transactionCategory === null) return null;
+
+  transactionCategory.deleted = true;
+
+  return transactionCategory.save();
+};
