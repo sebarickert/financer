03882947207e14584862d94fc605d35d979a8297
shamalainen <sebastian@hamse.fi>
2021-09-18 15:48:26 +0300

    Fix bug fixes that came to light

--- frontend/src/pages/accounts/AccountTransfer.tsx
+++ frontend/src/pages/accounts/AccountTransfer.tsx
@@ -9,18 +9,48 @@ import SEO from "../../components/seo/seo";
 import { getAllAccounts } from "./AccountService";
 import { addTransaction } from "../../services/TransactionService";
 import Container from "../../components/container/container";
+import {
+  getAllTransactionCategoriesWithCategoryTree,
+  ITransactionCategoryWithCategoryTree,
+} from "../profile/TransactionCategories/TransactionCategoriesService";
+import Button from "../../components/button/button";
+import { addTransactionCategoryMapping } from "../expenses/AddExpense";
 
 const AccountTransfer = (): JSX.Element => {
   const history = useHistory();
   const [errors, setErrors] = useState<string[]>([]);
   const [accountsRaw, setAccountsRaw] = useState<IAccount[] | null>(null);
   const [accounts, setAccounts] = useState<IOption[] | null>(null);
+  const [transactionCategoriesRaw, setTransactionCategoriesRaw] = useState<
+    ITransactionCategoryWithCategoryTree[] | null
+  >(null);
+  const [transactionCategories, setTransactionCategories] = useState<
+    IOption[] | null
+  >(null);
+
+  const [categoryAmount, setCategoryAmount] = useState<number[]>([]);
+  const addNewCategory = () =>
+    setCategoryAmount([
+      ...categoryAmount,
+      Math.max(...(categoryAmount.length === 0 ? [-1] : categoryAmount)) + 1,
+    ]);
+
+  const deleteTransactionCategoryItem = (itemToDelete: number) =>
+    setCategoryAmount(categoryAmount.filter((item) => item !== itemToDelete));
 
   useEffect(() => {
     const fetchAccounts = async () => {
       setAccountsRaw(await getAllAccounts());
     };
+
+    const fetchTransactionCategories = async () => {
+      setTransactionCategoriesRaw(
+        await getAllTransactionCategoriesWithCategoryTree()
+      );
+    };
+
     fetchAccounts();
+    fetchTransactionCategories();
   }, []);
 
   useEffect(() => {
@@ -34,6 +64,17 @@ const AccountTransfer = (): JSX.Element => {
     );
   }, [accountsRaw]);
 
+  useEffect(() => {
+    if (transactionCategoriesRaw === null) return;
+
+    setTransactionCategories(
+      transactionCategoriesRaw.map(({ _id, categoryTree }) => ({
+        value: _id,
+        label: categoryTree,
+      }))
+    );
+  }, [transactionCategoriesRaw]);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -46,13 +87,40 @@ const AccountTransfer = (): JSX.Element => {
       date: date.value,
     };
 
-    try {
-      const newTransaction = await addTransaction(newTransferData);
+    const transactionCategoryMappings: ITransactionCategoryMapping[] =
+      categoryAmount.map((item) => {
+        const newTransactionCategories =
+          event.target[`transactionCategory[${item}]category`];
+        const newTransactionCategoriesAmount =
+          event.target[`transactionCategory[${item}]amount`];
+        const newTransactionCategoriesDescription =
+          event.target[`transactionCategory[${item}]description`];
 
-      if (newTransaction.status === 201) {
+        return {
+          category_id: newTransactionCategories.value,
+          amount: parseFloat(
+            (newTransactionCategoriesAmount.value as string).replace(",", ".")
+          ),
+          description: newTransactionCategoriesDescription.value,
+        };
+      });
+
+    try {
+      const newTransactionJson = await addTransaction(newTransferData);
+      const newTransactionCategoryMappingJson =
+        await addTransactionCategoryMapping(
+          transactionCategoryMappings.map(
+            (newTransactionCategoryMappingData) => ({
+              ...newTransactionCategoryMappingData,
+              transaction_id: newTransactionJson.payload._id,
+            })
+          )
+        );
+
+      if (newTransactionJson.status === 201) {
         history.push("/accounts");
-      } else if (newTransaction.status === 400) {
-        setErrors(newTransaction?.errors || ["Unknown error."]);
+      } else if (newTransactionJson.status === 400) {
+        setErrors(newTransactionJson?.errors || ["Unknown error."]);
       }
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -60,7 +128,7 @@ const AccountTransfer = (): JSX.Element => {
     }
   };
 
-  return accounts === null ? (
+  return accounts === null || transactionCategories === null ? (
     <Loader loaderColor="blue" />
   ) : (
     <Container className="mt-6 sm:mt-12">
@@ -97,6 +165,63 @@ const AccountTransfer = (): JSX.Element => {
             The account to which the money will be added
           </Select>
         </div>
+        {transactionCategories.length > 0 && (
+          <>
+            <div className="border-t border-gray-200 pt-5 mt-8">
+              <h2 className="text-lg font-bold leading-7 text-gray-900 sm:text-2xl sm:leading-9 sm:truncate">
+                Categories
+              </h2>
+              <div className="grid gap-x-4 sm:grid-cols-2">
+                {categoryAmount.map((index) => (
+                  <div
+                    className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4"
+                    key={index}
+                  >
+                    <Select
+                      id={`transactionCategory[${index}]category`}
+                      options={transactionCategories}
+                      isRequired
+                    >
+                      Categories
+                    </Select>
+                    <Input
+                      id={`transactionCategory[${index}]amount`}
+                      help="Amount of the purchase."
+                      type="number"
+                      min={0.0}
+                      step={0.01}
+                      isCurrency
+                      isRequired
+                    >
+                      Amount
+                    </Input>
+                    <Input
+                      id={`transactionCategory[${index}]description`}
+                      help="Description of purchase, e.g. rent."
+                    >
+                      Description
+                    </Input>
+                    <Button
+                      className="mt-4"
+                      onClick={() => deleteTransactionCategoryItem(index)}
+                      accentColor="red"
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <Button
+              className="mt-4"
+              onClick={addNewCategory}
+              accentColor="green"
+            >
+              Add category
+            </Button>
+          </>
+        )}
       </Form>
     </Container>
   );
