8d3ae2dedcd5153fcc208fe6d783a0aaf200290f
shamalainen <sebastian@hamse.fi>
2021-09-19 13:05:38 +0300

    Create statistics page skeleton structure

---
+++ frontend/src/pages/statistics/Statistics.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SEO from "../../components/seo/seo";
+
+const Statistics = (): JSX.Element => {
+  return (
+    <>
+      <SEO title="Statistics" />
+      <h1>Statistics</h1>
+    </>
+  );
+};
+
+export default Statistics;
