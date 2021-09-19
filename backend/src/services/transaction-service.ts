e20837e2274a313497af14496743da4bd7eadc89
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2021-09-19 21:58:59 +0300

    Order transactions by date in backend

--- backend/src/services/transaction-service.ts
+++ backend/src/services/transaction-service.ts
@@ -5,20 +5,24 @@ import transactionModel, {
 export const findIncomeTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    user: userId,
-    toAccount: { $ne: undefined },
-    fromAccount: { $eq: undefined },
-  });
+  transactionModel
+    .find({
+      user: userId,
+      toAccount: { $ne: undefined },
+      fromAccount: { $eq: undefined },
+    })
+    .sort({ date: "asc" });
 
 export const findExpenseTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    user: userId,
-    fromAccount: { $ne: undefined },
-    toAccount: { $eq: undefined },
-  });
+  transactionModel
+    .find({
+      user: userId,
+      fromAccount: { $ne: undefined },
+      toAccount: { $eq: undefined },
+    })
+    .sort({ date: "asc" });
 
 export const findTransactionById = async (
   id: string
@@ -31,48 +35,56 @@ export const createTransaction = async (
 export const findTransferTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    user: userId,
-    fromAccount: { $ne: undefined },
-    toAccount: { $ne: undefined },
-  });
+  transactionModel
+    .find({
+      user: userId,
+      fromAccount: { $ne: undefined },
+      toAccount: { $ne: undefined },
+    })
+    .sort({ date: "asc" });
 
 export const findTransactionsByAccount = async (
   accountId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    $or: [
-      {
-        fromAccount: accountId,
-      },
-      {
-        toAccount: accountId,
-      },
-    ],
-  });
+  transactionModel
+    .find({
+      $or: [
+        {
+          fromAccount: accountId,
+        },
+        {
+          toAccount: accountId,
+        },
+      ],
+    })
+    .sort({ date: "asc" });
 
 export const findTransactionsAfterByAccount = async (
   accountId: string,
   date: Date
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    date: { $gt: date },
-    $or: [
-      {
-        fromAccount: accountId,
-      },
-      {
-        toAccount: accountId,
-      },
-    ],
-  });
+  transactionModel
+    .find({
+      date: { $gt: date },
+      $or: [
+        {
+          fromAccount: accountId,
+        },
+        {
+          toAccount: accountId,
+        },
+      ],
+    })
+    .sort({ date: "asc" });
 
 export const findTransactionsByUser = async (
   userId: string
 ): Promise<ITransactionModel[] | null> =>
-  transactionModel.find({
-    user: userId,
-  });
+  transactionModel
+    .find({
+      user: userId,
+    })
+    .sort({ date: "asc" });
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DANGER_truncateTransactionsByUser = async (
