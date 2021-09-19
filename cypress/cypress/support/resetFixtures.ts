1ed0279d429eb275f3bccdfa2359617d652e41e8
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2021-09-19 16:10:30 +0300

    Add eslint to cypress

--- cypress/cypress/support/resetFixtures.ts
+++ cypress/cypress/support/resetFixtures.ts
@@ -1,13 +1,14 @@
 beforeEach(() => {
-    // cy.visit('http://localhost:3000')
+  // cy.visit('http://localhost:3000')
 
-    cy.fixture("my-financer-data-20210918.json").then(fixture => fetch("http://localhost:3000/api/profile/my-data", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(fixture),
-      }));
-  })
-  
+  cy.fixture("my-financer-data-20210918.json").then((fixture) =>
+    fetch("http://localhost:3000/api/profile/my-data", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fixture),
+    })
+  );
+});
