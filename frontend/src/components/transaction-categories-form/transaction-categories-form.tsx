07da8755aa47462ddca795afeff093b3e90885ea
shamalainen <sebastian@hamse.fi>
2021-10-06 21:52:06 +0300

    Add allocated value logic
    Co-authored-by: Teemu Sillantaus <silte@users.noreply.github.com>

--- frontend/src/components/transaction-categories-form/transaction-categories-form.tsx
+++ frontend/src/components/transaction-categories-form/transaction-categories-form.tsx
@@ -4,30 +4,49 @@ import TransactionCategoriesFormItem from "./transaction-categories-form.item";
 
 interface ITransactionCategoriesFormProps {
   className?: string;
-  categoryAmount: number[];
+  categoryAmount: { [key in number]: number };
+  amountMaxValue: number;
   transactionCategories: IOption[];
   transactionCategoryMapping: ITransactionCategoryMapping[] | undefined;
   deleteTransactionCategoryItem(itemKey: number): void;
+  setTransactionCategoryItemAmount(itemKey: number, itemValue: number): void;
 }
 
 const TransactionCategoriesForm = ({
   className = "",
   categoryAmount,
   transactionCategories,
+  amountMaxValue,
   transactionCategoryMapping,
   deleteTransactionCategoryItem,
+  setTransactionCategoryItemAmount,
 }: ITransactionCategoriesFormProps): JSX.Element => {
+  const totalAllocatedAmount = Object.values(categoryAmount)
+    .filter((item) => !Number.isNaN(item))
+    .reduce((current, previous) => current + previous, 0);
+
   return (
     <div className={className}>
-      {categoryAmount.map((index, arrayIndex) => (
+      {Object.entries(categoryAmount).map(([index, value], arrayIndex) => (
         <TransactionCategoriesFormItem
-          categoryAmountIndex={index}
+          categoryAmountIndex={parseInt(index, 10)}
+          amountMaxValue={amountMaxValue}
           arrayIndex={arrayIndex}
           categories={transactionCategories}
           categoryMapping={transactionCategoryMapping}
           deleteTransactionCategoryItem={() =>
-            deleteTransactionCategoryItem(index)
+            deleteTransactionCategoryItem(parseInt(index, 10))
           }
+          setTransactionCategoryItemAmount={(newValue: number) =>
+            setTransactionCategoryItemAmount(parseInt(index, 10), newValue)
+          }
+          amountValue={value}
+          setUnallocatedAmount={() => {
+            setTransactionCategoryItemAmount(
+              parseInt(index, 10),
+              amountMaxValue - totalAllocatedAmount + (value || 0)
+            );
+          }}
         />
       ))}
     </div>
