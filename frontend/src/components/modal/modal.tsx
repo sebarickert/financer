ddd4a9ea09902c62990214f4b1a2024ea7cdb4d3
shamalainen <sebastian@hamse.fi>
2021-10-10 18:53:30 +0300

    Fix IOS bug

--- frontend/src/components/modal/modal.tsx
+++ frontend/src/components/modal/modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { isIOSDevice } from '../../utils/isIOSDevice';
 import { Button } from '../button/button';
 
 interface IModalProps {
@@ -24,7 +25,11 @@ export const Modal = ({
       </Button>
       {isOpen && (
         <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div
+            className={`flex items-end justify-center min-h-screen pt-4 px-4 text-center sm:block sm:p-0 ${
+              isIOSDevice() ? 'pb-28' : 'pb-20'
+            }`}
+          >
             <div className="fixed inset-0 transition-opacity">
               <div className="absolute inset-0 bg-gray-500 opacity-75" />
             </div>
