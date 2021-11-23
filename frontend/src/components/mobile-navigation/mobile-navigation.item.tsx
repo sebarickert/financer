d0f97e3bf714088f7ca51bdb1059666220db81cc
shamalainen <sebastian@hamse.fi>
2021-11-23 21:34:29 +0200

    Update mobile navigation look

--- frontend/src/components/mobile-navigation/mobile-navigation.item.tsx
+++ frontend/src/components/mobile-navigation/mobile-navigation.item.tsx
@@ -12,6 +12,7 @@ interface IMobileNavigationItemProps {
   isExact?: boolean;
   isActive?(match: never, location: { pathname: string }): boolean;
   hasDarkBackground?: boolean;
+  type?: 'standalone' | 'default';
 }
 
 export const MobileNavigationItem = ({
@@ -23,14 +24,40 @@ export const MobileNavigationItem = ({
   isExact,
   isActive,
   hasDarkBackground,
+  type = 'default',
 }: IMobileNavigationItemProps): JSX.Element => {
+  if (type === 'standalone') {
+    return (
+      <li>
+        <NavLink
+          to={link}
+          exact={isExact}
+          className="flex flex-col items-center justify-center focus:text-blue-financer hover:text-blue-financer pt-4 pb-3 after:h-1.5 after:w-1.5 after:bg-transparent after:rounded-full after:mt-1"
+          activeClassName="text-blue-financer after:!bg-blue-financer"
+          onClick={onClick}
+          aria-label={ariaLabel}
+          isActive={isActive}
+        >
+          <Icon type={iconName} />
+          <span
+            className={`text-xs mt-1 sr-only ${
+              hasDarkBackground ? 'text-gray-300' : 'text-gray-600'
+            }`}
+          >
+            {label}
+          </span>
+        </NavLink>
+      </li>
+    );
+  }
+
   return (
     <li>
       <NavLink
         to={link}
         exact={isExact}
-        className="flex flex-col items-center justify-center focus:text-blue-financer hover:text-blue-financer pb-2 pt-4"
-        activeClassName="text-blue-financer"
+        className="flex flex-col items-center justify-center focus:text-blue-financer hover:text-blue-financer pt-4 pb-2"
+        activeClassName="text-blue-financer after:!bg-blue-financer"
         onClick={onClick}
         aria-label={ariaLabel}
         isActive={isActive}
