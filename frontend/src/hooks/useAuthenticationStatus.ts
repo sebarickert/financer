dc167b48ea589291803e515c9b378ff496e25951
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-02-25 23:46:47 +0200

    Move authentication request to use hook

---
+++ frontend/src/hooks/useAuthenticationStatus.ts
@@ -0,0 +1,15 @@
+import { useQuery } from 'react-query';
+
+import { getAuthenticationStatus } from '../services/AuthenticationService';
+
+export const useAuthenticationStatus = () => {
+  const authenticationStatusQuery = useQuery(
+    'authenticationStatus',
+    getAuthenticationStatus,
+    {
+      staleTime: 300000,
+    }
+  );
+
+  return authenticationStatusQuery.data || null;
+};
