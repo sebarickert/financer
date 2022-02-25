739df4263c1b2e156212d7544e4a09f81d203f9b
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-02-25 22:39:02 +0200

    Move all transaction category requests to use hooks

---
+++ frontend/src/hooks/transactionCategories/useAddTransactionCategory.ts
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+import { useQueryClient } from 'react-query';
+
+import { addTransactionCategory } from '../../services/TransactionCategoriesService';
+
+export const useAddTransactionCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useCallback(
+    async (newTransactionCategoryData: ITransactionCategory) => {
+      const newTransactionCategory = await addTransactionCategory(
+        newTransactionCategoryData
+      );
+      queryClient.invalidateQueries('transactionCategories');
+
+      return newTransactionCategory;
+    },
+    [queryClient]
+  );
+};
---
+++ frontend/src/hooks/transactionCategories/useDeleteTransactionCategory.ts
@@ -0,0 +1,16 @@
+import { useCallback } from 'react';
+import { useQueryClient } from 'react-query';
+
+import { deleteTransactionCategory } from '../../services/TransactionCategoriesService';
+
+export const useDeleteTransactionCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useCallback(
+    async (id: string) => {
+      await deleteTransactionCategory(id);
+      queryClient.invalidateQueries('transactionCategories');
+    },
+    [queryClient]
+  );
+};
