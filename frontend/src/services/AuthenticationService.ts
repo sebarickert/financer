a42215b51a82661eeb2a79cfdec29f01d5ef5179
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-02-25 23:46:47 +0200

    Move authentication request to use hook

--- frontend/src/services/AuthenticationService.ts
+++ frontend/src/services/AuthenticationService.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export const getAuthenticationStatus =
   async (): Promise<IAuthenticationStatus> => {
     const authenticationStatus = await fetch('/auth/api/status');
