a4e4a37ba3dac4cd986cdd6c756529dc97ac110d
shamalainen <sebastian@hamse.fi>
2022-02-26 18:48:39 +0200

    Rename stuff

--- frontend/src/hooks/transfer/useEditTransfer.ts
+++ frontend/src/hooks/transfer/useEditTransfer.ts
@@ -1,17 +1,14 @@
 import { useCallback } from 'react';
 import { useQueryClient } from 'react-query';
 
-import { updateTransaction } from '../../services/TransactionService';
+import { updateTransfer } from '../../services/TransferService';
 
-export const useEditTransaction = () => {
+export const useEditTransfer = () => {
   const queryClient = useQueryClient();
 
   return useCallback(
     async (targetTransactionData: ITransaction, id: string) => {
-      const editedTransaction = await updateTransaction(
-        targetTransactionData,
-        id
-      );
+      const editedTransaction = await updateTransfer(targetTransactionData, id);
       queryClient.invalidateQueries('transfers');
       queryClient.invalidateQueries('transactions');
       queryClient.invalidateQueries('transactionCategoryMappings');
