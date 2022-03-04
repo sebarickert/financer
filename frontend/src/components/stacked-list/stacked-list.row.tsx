2e2b65ad53620d2898f39953e378b9968d2cbb3c
shamalainen <sebastian@hamse.fi>
2022-03-04 14:40:44 +0200

    Update categories page styles

--- frontend/src/components/stacked-list/stacked-list.row.tsx
+++ frontend/src/components/stacked-list/stacked-list.row.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from '../button/button';
 
-type Tag = { label: string; color: 'blue' | 'red' | 'green' };
 type Action = { label: string; color: 'blue' | 'red' | 'green'; link: string };
 
 export interface ICustomStackedListRowProps extends IStackedListRowProps {
@@ -11,10 +9,8 @@ export interface ICustomStackedListRowProps extends IStackedListRowProps {
 }
 export interface IStackedListRowProps {
   additionalInformation?: string[];
-  additionalLabel?: string;
   label: string;
   link?: string;
-  tags?: Tag[];
   id: string;
   actions?: Action[];
   testId?: string;
@@ -22,78 +18,47 @@ export interface IStackedListRowProps {
 
 export const StackedListRow = ({
   additionalInformation,
-  additionalLabel,
   label,
   link,
-  tags,
   actions,
   testId,
 }: IStackedListRowProps): JSX.Element => {
   const stackedListRowContent = (
-    <div className="px-4 py-4 flex items-center">
-      <div className="min-w-0 flex-1">
-        <h3 className="text-base font-medium truncate">{label}</h3>
+    <article className="relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500  grid grid-cols-[1fr,auto] gap-4 items-center">
+      <div className="flex flex-col justify-center overflow-hidden">
+        <h2 className="text-sm sm:text-base font-semibold truncate">{label}</h2>
         {additionalInformation && (
-          <div className="mt-1 flex flex-col sm:flex-row sm:items-center text-xs text-gray-500">
-            {additionalInformation.map((information, index) => (
-              <p
-                className={index !== 0 ? 'mt-1 sm:mt-0' : ''}
-                key={information}
-              >
-                <>
-                  {index !== 0 && (
-                    <span className="hidden sm:inline-block sm:mx-1">|</span>
-                  )}
+          <p className="text-xs sm:text-sm text-gray-500 mt-0.5 inline-flex flex-col">
+            {additionalInformation.map((information) => {
+              if (information === label) return null;
+
+              return (
+                <span key={information}>
                   {information.charAt(0).toUpperCase() + information.slice(1)}
-                </>
-              </p>
-            ))}
-          </div>
+                </span>
+              );
+            })}
+          </p>
         )}
       </div>
-      {(additionalLabel || actions) && (
-        <div className="flex-shrink-0 pl-4 text-right">
-          {tags && !actions && (
-            <div className="mb-1">
-              {tags.map(({ label: tagLabel, color }) => {
-                const colorMapping = {
-                  blue: 'bg-blue-100 text-blue-800',
-                  green: 'bg-emerald-100 text-emerald-800',
-                  red: 'bg-red-100 text-red-800',
-                };
-
-                const baseClasses = [
-                  'px-2 inline-flex text-xs leading-5 font-semibold rounded-full',
-                ];
-
-                baseClasses.push(colorMapping[color]);
-
-                return (
-                  <p className={baseClasses.join(' ')} key={tagLabel}>
-                    {tagLabel}
-                  </p>
-                );
-              })}
-            </div>
+      {actions && (
+        <div>
+          {actions.map(
+            ({ label: actionLabel, color, link: actionLink }, index) => (
+              <Button
+                accentColor={color}
+                className={`${index > 0 ? 'ml-3' : ''}`}
+                // eslint-disable-next-line react/no-array-index-key
+                key={`action-button-${index}`}
+                link={actionLink}
+              >
+                {actionLabel}
+              </Button>
+            )
           )}
-          {actions &&
-            actions.map(
-              ({ label: actionLabel, color, link: actionLink }, index) => (
-                <Button
-                  accentColor={color}
-                  className={`${index > 0 ? 'ml-3' : ''}`}
-                  // eslint-disable-next-line react/no-array-index-key
-                  key={`action-button-${index}`}
-                  link={actionLink}
-                >
-                  {actionLabel}
-                </Button>
-              )
-            )}
-          {!actions && <p className="font-medium">{additionalLabel}</p>}
         </div>
       )}
-    </div>
+    </article>
   );
 
   return (
