f0ae8897abf01f561e5da9f9f16d52821c9e6cdb
shamalainen <sebastian@hamse.fi>
2022-03-15 20:52:29 +0200

    Fix console errors

--- packages/frontend/src/components/icon/icon.arrowLeft.tsx
+++ packages/frontend/src/components/icon/icon.arrowLeft.tsx
@@ -16,9 +16,9 @@ export const IconArrowLeft = ({
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M10 19l-7-7m0 0l7-7m-7 7h18"
       />
     </svg>
