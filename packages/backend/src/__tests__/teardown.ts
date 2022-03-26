ca627f7664de1362e43a8b06a8e038261a842a0f
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-03-26 20:08:35 +0200

    Replace old express backend with new backend-nest

--- packages/backend/src/__tests__/teardown.ts
+++
@@ -1,4 +0,0 @@
-import { memoryDatabaseServer } from '../config/MemoryDatabaseServer';
-
-// eslint-disable-next-line import/no-default-export
-export default async (): Promise<boolean> => memoryDatabaseServer.stop();
