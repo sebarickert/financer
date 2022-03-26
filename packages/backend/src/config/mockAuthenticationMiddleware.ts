4f77742e06d969ea956173ec48969c572d8c203d
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-03-26 20:08:35 +0200

    Replace old express backend with new backend-nest

--- packages/backend/src/config/mockAuthenticationMiddleware.ts
+++ packages/backend/src/config/mockAuthenticationMiddleware.ts
@@ -1,16 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const DUMMY_TEST_USER = {
-  roles: ['test-user'],
+import { Role, UserDocument } from '../modules/users/schemas/user.schema';
+
+export const DUMMY_TEST_USER: Partial<UserDocument> = {
+  roles: [Role.testUser],
   _id: '61460d7354ea082ad0256749',
-  id: '61460d7354ea082ad0256749',
   name: 'Dummy Test User',
 };
 
 export const mockAuthenticationMiddleware = (
   req: Request,
   _: Response,
-  next: NextFunction
+  next: NextFunction,
 ): void => {
   req.user = DUMMY_TEST_USER;
   req.isAuthenticated = () => true;
--- packages/backend/src/utils/fileExists.ts
+++
@@ -1,5 +0,0 @@
-import { existsSync } from 'fs';
-import path from 'path';
-
-export const fileExists = (filename: string): boolean =>
-  existsSync(path.join(filename));
