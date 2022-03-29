889edbc2e044277e0944806668286ecf5bacbfbe
shamalainen <sebastian@hamse.fi>
2022-03-29 22:20:07 +0300

    Add date to form

--- packages/frontend/src/pages/accounts/account-modals/AccountUpdateMarketValueModal.tsx
+++ packages/frontend/src/pages/accounts/account-modals/AccountUpdateMarketValueModal.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 import { Input } from '../../../components/input/input';
 import { ModalCustom } from '../../../components/modal/custom/modal.custom';
+import { inputDateFormat } from '../../../utils/formatDate';
 
 interface IAccountUpdateMarketValueModalProps {
-  handleUpdate(newMarketValue: number): void;
+  handleUpdate(newMarketValue: number, newDate: Date): void;
   currentValue: number;
 }
 
@@ -13,26 +14,46 @@ export const AccountUpdateMarketValueModal = ({
   handleUpdate,
 }: IAccountUpdateMarketValueModalProps) => {
   const [newMarketValue, setNewMarketValue] = useState(currentValue);
+  const [newDate, setNewDate] = useState(new Date());
 
-  const handleInputUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMarketValueInputUpdate = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setNewMarketValue(Number(event.target.value));
+  };
+
+  const handleDateInputUpdate = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewDate(new Date(event.target.value));
   };
 
   return (
     <ModalCustom
       submitButtonLabel="Update"
       modalOpenButtonLabel="Update current market value"
-      onConfirm={() => handleUpdate(newMarketValue)}
+      onConfirm={() => handleUpdate(newMarketValue, newDate)}
     >
-      <Input
-        id="currentMarketValue"
-        type="number"
-        isRequired
-        value={currentValue}
-        onChange={handleInputUpdate}
-      >
-        Current market value
-      </Input>
+      <div className="space-y-4">
+        <Input
+          id="currentMarketValue"
+          type="number"
+          isRequired
+          value={currentValue}
+          onChange={handleMarketValueInputUpdate}
+        >
+          Current market value
+        </Input>
+        <Input
+          id="date"
+          type="datetime-local"
+          value={inputDateFormat(new Date())}
+          onChange={handleDateInputUpdate}
+          isDate
+        >
+          Date
+        </Input>
+      </div>
     </ModalCustom>
   );
 };
