abd942e893da13a4807a930853a3c362c4367b64
shamalainen <sebastian@hamse.fi>
2022-03-30 21:35:14 +0300

    nice stuff

--- packages/frontend/src/pages/profile/UserPreferences/UserPreferencesRouter.tsx
+++ packages/frontend/src/pages/profile/UserPreferences/UserPreferencesRouter.tsx
@@ -1,11 +1,41 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { UserDefaultExpenseAccount } from './preferences/UserDefaultExpenseAccount';
+import { UserDefaultIncomeAccount } from './preferences/UserDefaultIncomeAccount';
+import { UserDefaultMarketUpdateSettings } from './preferences/UserDefaultMarketUpdateSettings';
+import { UserDefaultTransferSourceAccount } from './preferences/UserDefaultTransferSourceAccount';
+import { UserDefaultTransferTargetAccount } from './preferences/UserDefaultTransferTargetAccount';
+import { UserTransactionListChunkSize } from './preferences/UserTransactionListChunkSize';
 import { UserPreferences } from './UserPreferences';
 
 export const UserPreferencesRouter = (): JSX.Element => {
   return (
     <Routes>
       <Route index element={<UserPreferences />} />
+      <Route
+        path="default-income-account"
+        element={<UserDefaultIncomeAccount />}
+      />
+      <Route
+        path="default-expense-account"
+        element={<UserDefaultExpenseAccount />}
+      />
+      <Route
+        path="default-transfer-source-account"
+        element={<UserDefaultTransferSourceAccount />}
+      />
+      <Route
+        path="default-transfer-target-account"
+        element={<UserDefaultTransferTargetAccount />}
+      />
+      <Route
+        path="transaction-list-chunk-size"
+        element={<UserTransactionListChunkSize />}
+      />
+      <Route
+        path="market-update-settings"
+        element={<UserDefaultMarketUpdateSettings />}
+      />
     </Routes>
   );
 };
--- packages/frontend/src/pages/profile/UserPreferences/preferences/UserDefaultTransferTargetAccount.tsx
+++ packages/frontend/src/pages/profile/UserPreferences/preferences/UserDefaultTransferTargetAccount.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
+import { Heading } from '../../../../components/heading/heading';
+import { Icon } from '../../../../components/icon/icon';
 import { Loader } from '../../../../components/loader/loader';
 import { ModalCustom } from '../../../../components/modal/custom/modal.custom';
 import { Select } from '../../../../components/select/select';
@@ -23,23 +26,43 @@ export const UserDefaultTransferTargetAccount = (): JSX.Element => {
   return accounts === null ? (
     <Loader loaderColor="blue" />
   ) : (
-    <ModalCustom
-      modalOpenButtonLabel="Set default transfer target account"
-      onConfirm={handleSave}
-      submitButtonLabel="Save"
-    >
-      <Select
-        id="toAccount"
-        options={accounts.map(({ name, _id }) => ({
-          label: name,
-          value: _id,
-        }))}
-        defaultValue={defaultTransferTargetAccount}
-        isRequired
-        handleOnChange={handleSelectChange}
+    <>
+      <div className="bg-blue-financer -mx-6 -mt-8 text-center pt-6 pb-4 mb-6 px-6 relative">
+        <NavLink
+          to={'/profile/user-preferences'}
+          className="absolute left-6 top-1/2 -translate-y-1/2 pt-2"
+        >
+          <span className="sr-only">Go back</span>
+          <Icon
+            type={'chevron-left'}
+            className="stroke-white -translate-x-1/2"
+          />
+        </NavLink>
+        <Heading
+          variant="h1"
+          className="!text-base !tracking-tight !text-white"
+        >
+          Default transfer target account
+        </Heading>
+      </div>
+      <ModalCustom
+        modalOpenButtonLabel="Set default transfer target account"
+        onConfirm={handleSave}
+        submitButtonLabel="Save"
       >
-        Default transfer target account
-      </Select>
-    </ModalCustom>
+        <Select
+          id="toAccount"
+          options={accounts.map(({ name, _id }) => ({
+            label: name,
+            value: _id,
+          }))}
+          defaultValue={defaultTransferTargetAccount}
+          isRequired
+          handleOnChange={handleSelectChange}
+        >
+          Default transfer target account
+        </Select>
+      </ModalCustom>
+    </>
   );
 };
