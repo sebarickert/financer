e361fb8163279965270c144ef7c7b9b64202857b
shamalainen <sebastian@hamse.fi>
2022-04-02 13:57:20 +0300

    Fix tests

--- packages/frontend/src/components/link-list/link-list.button.tsx
+++ packages/frontend/src/components/link-list/link-list.button.tsx
@@ -4,24 +4,32 @@ interface LinkListButtonProps {
   icon?: IconName;
   children: string;
   handleClick(): void;
+  testId?: string;
 }
 
 export const LinkListButton = ({
   icon,
   children,
   handleClick,
+  testId,
 }: LinkListButtonProps): JSX.Element => {
   return (
     <button
       className="relative flex gap-4 items-center focus-within:bg-gray-200 hover:bg-gray-200 overflow-hidden pl-4 w-full"
       onClick={handleClick}
+      data-testid={testId}
     >
-      {icon && <Icon type={icon} className="stroke-black flex-shrink-0" />}
-      <span className="text-base flex justify-between font-semibold tracking-tight py-4 pr-4 after:h-[1px] after:w-full after:absolute after:bg-gray-200 after:bottom-0 group-last:after:hidden flex-1 overflow-hidden ">
+      {icon && (
+        <Icon
+          type={icon}
+          className="stroke-black flex-shrink-0 pointer-events-none"
+        />
+      )}
+      <span className="text-base flex justify-between font-semibold tracking-tight py-4 pr-4 after:h-[1px] after:w-full after:absolute after:bg-gray-200 after:bottom-0 group-last:after:hidden flex-1 overflow-hidden">
         <span className="truncate">{children}</span>
         <Icon
           type={'chevron-right'}
-          className=" stroke-gray-300 flex-shrink-0"
+          className=" stroke-gray-300 flex-shrink-0 pointer-events-none"
         />
       </span>
     </button>
