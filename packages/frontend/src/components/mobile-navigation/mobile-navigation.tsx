96f0a5d1ea5f824f1f806b904515466bca9e41ec
shamalainen <sebastian@hamse.fi>
2022-04-04 21:56:21 +0300

    moard fixes

--- packages/frontend/src/components/mobile-navigation/mobile-navigation.tsx
+++ packages/frontend/src/components/mobile-navigation/mobile-navigation.tsx
@@ -3,7 +3,7 @@ import { MobileNavigationItem } from './mobile-navigation.item';
 
 export const MobileNavigation = (): JSX.Element => {
   return (
-    <div className="fixed bottom-[env(safe-area-inset-bottom)] left-0 right-0 z-10 w-full bg-white border-t">
+    <div className="fixed bottom-0 pb-safe left-0 right-0 z-10 w-full bg-white border-t">
       <nav aria-label="Main navigation in mobile viewmode.">
         <ul className={`grid grid-cols-5 relative`}>
           <MobileNavigationItem
