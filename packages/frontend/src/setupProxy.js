4266008d492a826cba13e6a0a389defff37feaf0
shamalainen <sebastian@hamse.fi>
2022-04-12 21:47:41 +0300

    Add kubernetes settings

--- packages/frontend/src/setupProxy.js
+++ packages/frontend/src/setupProxy.js
@@ -4,6 +4,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
+    '/health-check',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:4000',
