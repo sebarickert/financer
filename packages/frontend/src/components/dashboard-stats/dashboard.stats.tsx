725946b2033315e6d7e6812c68fed98185022bd0
shamalainen <sebastian@hamse.fi>
2022-04-14 23:07:17 +0300

    Update dashboard stats to use proper component

--- packages/frontend/src/components/dashboard-stats/dashboard.stats.tsx
+++ packages/frontend/src/components/dashboard-stats/dashboard.stats.tsx
@@ -2,6 +2,8 @@ import { useCurrentMonthExpensesTotalAmount } from '../../hooks/expense/useAllEx
 import { useCurrentMonthIncomesTotalAmount } from '../../hooks/income/useAllIncomes';
 import { useTotalBalance } from '../../hooks/useTotalBalance';
 import { formatCurrency } from '../../utils/formatCurrency';
+import { DescriptionList } from '../description-list/description-list';
+import { DescriptionListItem } from '../description-list/description-list.item';
 
 interface IDashboardStatsProps {
   className?: string;
@@ -15,33 +17,16 @@ export const DashboardStats = ({
   const totalExpenses = useCurrentMonthExpensesTotalAmount();
 
   return (
-    <section className={`bg-gray-25 border rounded-lg ${className}`}>
-      <dl className="relative px-6 pt-10 pb-6 border-b">
-        <dt className="absolute text-sm font-medium text-gray-700 truncate lg:text-base top-4 left-6">
-          Balance
-        </dt>
-        <dd className="text-3xl font-bold tracking-tight">
-          {Number.isNaN(totalBalance) ? '-' : formatCurrency(totalBalance)}
-        </dd>
-      </dl>
-      <section className="grid grid-cols-2 divide-x">
-        <dl className="py-4 pl-6 pr-4">
-          <dt className="text-xs font-medium text-gray-700 truncate lg:text-sm">
-            Income
-          </dt>
-          <dd className="text-xl font-bold tracking-tight">
-            {Number.isNaN(totalIncomes) ? '-' : formatCurrency(totalIncomes)}
-          </dd>
-        </dl>
-        <dl className="py-4 pl-6 pr-4">
-          <dt className="text-xs font-medium text-gray-700 truncate lg:text-sm">
-            Expenses
-          </dt>
-          <dd className="text-xl font-bold tracking-tight">
-            {Number.isNaN(totalExpenses) ? '-' : formatCurrency(totalExpenses)}
-          </dd>
-        </dl>
-      </section>
-    </section>
+    <DescriptionList className={className}>
+      <DescriptionListItem label="Balance" isLarge>
+        {Number.isNaN(totalBalance) ? '-' : formatCurrency(totalBalance)}
+      </DescriptionListItem>
+      <DescriptionListItem label="Income">
+        {Number.isNaN(totalIncomes) ? '-' : formatCurrency(totalIncomes)}
+      </DescriptionListItem>
+      <DescriptionListItem label="Expenses">
+        {Number.isNaN(totalExpenses) ? '-' : formatCurrency(totalExpenses)}
+      </DescriptionListItem>
+    </DescriptionList>
   );
 };
--- packages/frontend/src/components/description-list/description-list.item.tsx
+++ packages/frontend/src/components/description-list/description-list.item.tsx
@@ -2,20 +2,28 @@ export interface IDescriptionListItemProps {
   label: string;
   children: string;
   testId?: string;
+  isLarge?: boolean;
 }
 
 export const DescriptionListItem = ({
   label,
   children,
   testId,
+  isLarge,
 }: IDescriptionListItemProps): JSX.Element => {
   return (
     <>
-      <dt className="text-xs font-medium text-gray-700 truncate lg:text-sm">
+      <dt
+        className={`${
+          isLarge ? 'text-sm lg:text-base' : 'text-xs lg:text-sm'
+        } font-medium text-gray-700 truncate`}
+      >
         {label}
       </dt>
       <dd
-        className="text-xl font-bold tracking-tight truncate"
+        className={`font-bold tracking-tight truncate ${
+          isLarge ? 'text-3xl' : 'text-xl'
+        }`}
         data-testid={testId}
       >
         {children}
