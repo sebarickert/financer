e8f6623d55a53faeff6eea3c902e72cdbdf2d681
shamalainen <sebastian@hamse.fi>
2022-04-25 19:35:53 +0300

    Update account page

--- packages/frontend/src/components/description-list/description-list.item.tsx
+++ packages/frontend/src/components/description-list/description-list.item.tsx
@@ -1,6 +1,8 @@
+import { ReactNode } from 'react';
+
 export interface IDescriptionListItemProps {
   label: string;
-  children: string;
+  children: string | ReactNode;
   testId?: string;
   isLarge?: boolean;
 }
