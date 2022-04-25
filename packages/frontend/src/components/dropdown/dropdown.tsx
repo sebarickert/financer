67ef0370bf6af92d8be8758d326c9678583e50d1
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-04-25 21:54:10 +0300

    Fix stats page filter

--- packages/frontend/src/components/dropdown/dropdown.tsx
+++ packages/frontend/src/components/dropdown/dropdown.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 import { Icon, IconName } from '../icon/icon';
 
 import { DropdownBody } from './dropdown.body';
@@ -11,37 +12,6 @@ interface IDropdownProps {
   label: string;
   items: DropdownItemType[];
   className?: string;
-}
-
-function useOnClickOutside(
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  ref: any,
-  handler: any,
-  secondaryRef: any = undefined
-  /* eslint-enable @typescript-eslint/no-explicit-any */
-) {
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const listener = (event: any) => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      if (
-        typeof secondaryRef !== 'undefined' &&
-        (!secondaryRef.current || secondaryRef.current.contains(event.target))
-      ) {
-        return;
-      }
-
-      handler(event);
-    };
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
-    return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
-    };
-  }, [ref, secondaryRef, handler]);
 }
 
 export const Dropdown = ({
