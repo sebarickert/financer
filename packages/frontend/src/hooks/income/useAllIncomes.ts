85cbf93294741bd40ff6e03f3fbfac58adcdf6a8
shamalainen <sebastian@hamse.fi>
2022-04-25 21:26:30 +0300

    Update frontpage dashboard stats to use proper hooks

--- packages/frontend/src/hooks/income/useAllIncomes.ts
+++ packages/frontend/src/hooks/income/useAllIncomes.ts
@@ -51,34 +51,6 @@ export const useAllIncomesPaged = (
   };
 };
 
-export const useCurrentMonthIncomesTotalAmount = (): number => {
-  const incomes = useAllIncomes();
-  const [totalAmount, setTotalAmount] = useState(NaN);
-
-  useEffect(() => {
-    if (incomes === null) return;
-
-    const total = incomes.reduce((currentTotal, { amount, date }) => {
-      const currentMonth = new Date().getMonth() + 1;
-      const month = new Date(date).getMonth() + 1;
-      const currentYear = new Date().getFullYear();
-
-      if (
-        currentMonth === month &&
-        currentYear === new Date(date).getFullYear()
-      ) {
-        return currentTotal + amount;
-      }
-
-      return currentTotal;
-    }, 0);
-
-    setTotalAmount(total);
-  }, [incomes]);
-
-  return totalAmount;
-};
-
 export const useAllIncomesGroupByMonth = (
   initialForbiddenAccountTypes: AccountType[] = []
 ): [IncomesPerMonth[], React.Dispatch<React.SetStateAction<AccountType[]>>] => {
