a3c8a1f767c547f5f820268fe94dbef3d8b79908
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-07-06 19:48:21 +0300

    Add TransactionTemplate DTOs

---
+++ packages/types/src/transaction-template/index.ts
@@ -0,0 +1,4 @@
+export * from './transaction-template.dto';
+export * from './create-transaction-template.dto';
+export * from './update-transaction-template.dto';
+export * from './transaction-template-type';
---
+++ packages/types/src/transaction-template/transaction-template.dto.ts
@@ -0,0 +1,55 @@
+import {
+  IsMongoId,
+  Min,
+  IsNotEmpty,
+  IsString,
+  IsDateString,
+  ValidateNested,
+  IsEnum,
+} from 'class-validator';
+
+import { TransactionType } from '../transaction/transaction-type';
+
+import { TransactionTemplateType } from './transaction-template-type';
+
+export class TransactionTemplateDto<ObjectIdType = string> {
+  @IsMongoId()
+  readonly _id: ObjectIdType;
+
+  @IsNotEmpty({ message: 'Template name must not be empty.' })
+  @IsString()
+  readonly templateName: string;
+
+  @IsEnum(TransactionTemplateType, {
+    each: true,
+    message: 'Type must defined.',
+  })
+  readonly templateType: TransactionTemplateType[];
+
+  @IsEnum(TransactionType, {
+    message: 'Type must defined.',
+  })
+  readonly templateVisibility: TransactionType;
+
+  @IsMongoId()
+  readonly fromAccount: ObjectIdType;
+
+  @IsMongoId()
+  readonly toAccount: ObjectIdType;
+
+  @Min(0.01, { message: 'Amount must be a positive number.' })
+  readonly amount: number;
+
+  @IsNotEmpty({ message: 'Description must not be empty.' })
+  @IsString()
+  readonly description: string;
+
+  @IsDateString({}, { message: 'Date must not be empty.' })
+  readonly dayOfMonth: Date;
+
+  @IsMongoId()
+  readonly userId: ObjectIdType;
+
+  @ValidateNested({ each: true })
+  categories: ObjectIdType[];
+}
