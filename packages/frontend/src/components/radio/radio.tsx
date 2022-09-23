606fde86fd0b424e034307dc216c0b2701114997
shamalainen <sebastian@hamse.fi>
2022-09-23 17:46:28 +0300

    Fix dark mode

--- packages/frontend/src/components/radio/radio.tsx
+++ packages/frontend/src/components/radio/radio.tsx
@@ -17,7 +17,7 @@ export const Radio = ({
 }: RadioProps): JSX.Element => {
   return (
     <label
-      className={`flex items-center p-4 gap-4 rounded hover:bg-gray-100 dark:hover:bg-gray-600 hover:cursor-pointer ${className}`}
+      className={`flex items-center p-4 gap-4 rounded hover:bg-gray-100 hover:cursor-pointer ${className}`}
       htmlFor={`${name}-${value}`}
     >
       <div className="flex items-center h-5">
@@ -26,13 +26,11 @@ export const Radio = ({
           name={name}
           type="radio"
           value={value}
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
           defaultChecked={isChecked}
         />
       </div>
-      <span className="text-base font-medium text-gray-900 dark:text-gray-300">
-        {children}
-      </span>
+      <span className="text-base font-medium text-gray-900">{children}</span>
     </label>
   );
 };
