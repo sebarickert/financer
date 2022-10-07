c85b6361750090b0eec9b6f79220b80f3e6d3e3f
shamalainen <sebastian@hamse.fi>
2022-10-07 21:34:20 +0300

    Update variable naming

--- packages/frontend/src/components/mobile-navigation/mobile-navigation.actions.body.tsx
+++ packages/frontend/src/components/mobile-navigation/mobile-navigation.actions.body.tsx
@@ -1,5 +1,4 @@
 import { Transition } from '@headlessui/react';
-import React from 'react';
 
 import { IconName } from '../icon/icon';
 
@@ -18,49 +17,51 @@ export const MobileNavigationActionsBody = ({
   onClick = () => {},
 }: IMobileNavigationActionsBodyProps): JSX.Element => {
   return (
-    <Transition
-      show={!isModalHidden}
-      enter="transition ease-out duration-100"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-      className="absolute bottom-full -translate-y-3 left-0 right-0 flex justify-center"
-      aria-hidden={isModalHidden}
-    >
-      <nav
-        className="bg-black-off text-white p-2 rounded-lg shadow-md whitespace-nowrap mx-4 max-w-sm w-full"
-        ref={outsideClickRef}
-        aria-label="Quick transaction actions navigation in mobile viewmode."
+    <>
+      <Transition
+        show={isModalHidden}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+        className="absolute bottom-full -translate-y-3 left-0 right-0 flex justify-center"
+        aria-hidden={isModalHidden}
       >
-        <ul className="grid grid-cols-3 justify-center items-center">
-          <MobileNavigationItem
-            label="Income"
-            iconName={IconName.download}
-            link="/statistics/incomes/add"
-            ariaLabel="Add new income transaction"
-            onClick={() => onClick(true)}
-            hasDarkBackground
-          />
-          <MobileNavigationItem
-            label="Expense"
-            iconName={IconName.upload}
-            link="/statistics/expenses/add"
-            ariaLabel="Add new expense transaction"
-            onClick={() => onClick(true)}
-            hasDarkBackground
-          />
-          <MobileNavigationItem
-            label="Transfer"
-            iconName={IconName.switchHorizontal}
-            link="/statistics/transfers/add"
-            ariaLabel="Add new transfer transaction"
-            onClick={() => onClick(true)}
-            hasDarkBackground
-          />
-        </ul>
-      </nav>
-    </Transition>
+        <nav
+          className="bg-black-off text-white p-2 rounded-lg shadow-md whitespace-nowrap mx-4 max-w-sm w-full"
+          ref={outsideClickRef}
+          aria-label="Quick transaction actions navigation in mobile viewmode."
+        >
+          <ul className="grid grid-cols-3 justify-center items-center">
+            <MobileNavigationItem
+              label="Income"
+              iconName={IconName.download}
+              link="/statistics/incomes/add"
+              ariaLabel="Add new income transaction"
+              onClick={() => onClick(false)}
+              hasDarkBackground
+            />
+            <MobileNavigationItem
+              label="Expense"
+              iconName={IconName.upload}
+              link="/statistics/expenses/add"
+              ariaLabel="Add new expense transaction"
+              onClick={() => onClick(false)}
+              hasDarkBackground
+            />
+            <MobileNavigationItem
+              label="Transfer"
+              iconName={IconName.switchHorizontal}
+              link="/statistics/transfers/add"
+              ariaLabel="Add new transfer transaction"
+              onClick={() => onClick(false)}
+              hasDarkBackground
+            />
+          </ul>
+        </nav>
+      </Transition>
+    </>
   );
 };
