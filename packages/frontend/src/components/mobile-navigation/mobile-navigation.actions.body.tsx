43ae4368875e729cb8506289a1e8d41e9fc9258b
shamalainen <sebastian@hamse.fi>
2022-10-08 18:09:07 +0300

    Remove fragment

--- packages/frontend/src/components/mobile-navigation/mobile-navigation.actions.body.tsx
+++ packages/frontend/src/components/mobile-navigation/mobile-navigation.actions.body.tsx
@@ -17,51 +17,49 @@ export const MobileNavigationActionsBody = ({
   onClick = () => {},
 }: IMobileNavigationActionsBodyProps): JSX.Element => {
   return (
-    <>
-      <Transition
-        show={isModalHidden}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-        className="absolute bottom-full -translate-y-3 left-0 right-0 flex justify-center"
-        aria-hidden={isModalHidden}
+    <Transition
+      show={isModalHidden}
+      enter="transition ease-out duration-100"
+      enterFrom="transform opacity-0 scale-95"
+      enterTo="transform opacity-100 scale-100"
+      leave="transition ease-in duration-75"
+      leaveFrom="transform opacity-100 scale-100"
+      leaveTo="transform opacity-0 scale-95"
+      className="absolute bottom-full -translate-y-3 left-0 right-0 flex justify-center"
+      aria-hidden={isModalHidden}
+    >
+      <nav
+        className="bg-black-off text-white p-2 rounded-lg shadow-md whitespace-nowrap mx-4 max-w-sm w-full"
+        ref={outsideClickRef}
+        aria-label="Quick transaction actions navigation in mobile viewmode."
       >
-        <nav
-          className="bg-black-off text-white p-2 rounded-lg shadow-md whitespace-nowrap mx-4 max-w-sm w-full"
-          ref={outsideClickRef}
-          aria-label="Quick transaction actions navigation in mobile viewmode."
-        >
-          <ul className="grid grid-cols-3 justify-center items-center">
-            <MobileNavigationItem
-              label="Income"
-              iconName={IconName.download}
-              link="/statistics/incomes/add"
-              ariaLabel="Add new income transaction"
-              onClick={() => onClick(false)}
-              hasDarkBackground
-            />
-            <MobileNavigationItem
-              label="Expense"
-              iconName={IconName.upload}
-              link="/statistics/expenses/add"
-              ariaLabel="Add new expense transaction"
-              onClick={() => onClick(false)}
-              hasDarkBackground
-            />
-            <MobileNavigationItem
-              label="Transfer"
-              iconName={IconName.switchHorizontal}
-              link="/statistics/transfers/add"
-              ariaLabel="Add new transfer transaction"
-              onClick={() => onClick(false)}
-              hasDarkBackground
-            />
-          </ul>
-        </nav>
-      </Transition>
-    </>
+        <ul className="grid grid-cols-3 justify-center items-center">
+          <MobileNavigationItem
+            label="Income"
+            iconName={IconName.download}
+            link="/statistics/incomes/add"
+            ariaLabel="Add new income transaction"
+            onClick={() => onClick(false)}
+            hasDarkBackground
+          />
+          <MobileNavigationItem
+            label="Expense"
+            iconName={IconName.upload}
+            link="/statistics/expenses/add"
+            ariaLabel="Add new expense transaction"
+            onClick={() => onClick(false)}
+            hasDarkBackground
+          />
+          <MobileNavigationItem
+            label="Transfer"
+            iconName={IconName.switchHorizontal}
+            link="/statistics/transfers/add"
+            ariaLabel="Add new transfer transaction"
+            onClick={() => onClick(false)}
+            hasDarkBackground
+          />
+        </ul>
+      </nav>
+    </Transition>
   );
 };
