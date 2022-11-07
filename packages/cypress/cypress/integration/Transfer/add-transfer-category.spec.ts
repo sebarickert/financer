e4fc06e9ccf269f90760b577667e775c4febe921
shamalainen <sebastian@hamse.fi>
2022-11-07 22:18:59 +0200

    Fix tests

--- packages/cypress/cypress/integration/Transfer/add-transfer-category.spec.ts
+++ packages/cypress/cypress/integration/Transfer/add-transfer-category.spec.ts
@@ -23,11 +23,6 @@ describe('Add transfer with category', () => {
     cy.location('pathname').should('not.contain', '/add');
     cy.contains(TRANSFER_NAME).click();
 
-    cy.getById('categories-wrapper_list-header').should(
-      'contain.text',
-      'Categories'
-    );
-    cy.getById('categories-wrapper_list-body').should('have.length', 1);
     cy.getById('category_label').should(
       'contain.text',
       'Category for all types'
