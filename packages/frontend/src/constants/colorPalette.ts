c897804acab45bc18311036f76024ee8afe5233a
shamalainen-houston <sebastian.hamalainen@houston-inc.com>
2022-11-10 08:32:00 +0200

    Update colorPalette structure

--- packages/frontend/src/constants/colorPalette.ts
+++ packages/frontend/src/constants/colorPalette.ts
@@ -1,8 +1,2 @@
-export const colorPalette = {
-  red: '#dc2626',
-  green: '#059669',
-  blue: '#0065ff',
-  charcoal: '#202626',
-  gray: '#fafafa',
-  'gray-dark': '#f2f2f2',
-};
+import colorPalette from './colorPalette.json';
+export { colorPalette };
