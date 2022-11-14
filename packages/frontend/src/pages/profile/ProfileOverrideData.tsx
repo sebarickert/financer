e7f3b6a9dd0ed6f38d05d7052a856a93adddb468
shamalainen <sebastian@hamse.fi>
2022-11-14 19:54:23 +0200

    Update override page

--- packages/frontend/src/pages/profile/ProfileOverrideData.tsx
+++ packages/frontend/src/pages/profile/ProfileOverrideData.tsx
@@ -2,8 +2,8 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 
 import { Button } from '../../components/button/button';
-import { DescriptionList } from '../../components/description-list/description-list';
-import { DescriptionListItem } from '../../components/description-list/description-list.item';
+import { Heading } from '../../components/heading/heading';
+import { InfoCard } from '../../components/info-card/info-card';
 import {
   Notification,
   INotificationProps,
@@ -122,34 +122,36 @@ export const ProfileOverrideData = (): JSX.Element => {
           {notification.children}
         </Notification>
       )}
-      <div>
-        <label
-          htmlFor="selectFiles"
-          className="inline-flex items-center justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md sm:w-auto focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-blue-500 active:bg-blue-700 focus:ring-blue-500"
-        >
-          Choose file
-          <input
-            className="hidden"
-            type="file"
-            id="selectFiles"
-            onChange={handleFileChange}
-            accept="application/json"
-          />
-        </label>
-        <span className="ml-2">{overrideFilename || 'No file selected'}</span>
-      </div>
-      <DescriptionList label="Override data details" className="mt-6">
-        <DescriptionListItem label="Account count">
-          {overrideAccountCount ? `${overrideAccountCount}` : '-'}
-        </DescriptionListItem>
-        <DescriptionListItem label="Transaction count">
-          {overrideTranactionCount ? `${overrideTranactionCount}` : '-'}
-        </DescriptionListItem>
-      </DescriptionList>
-
-      <Button onClick={handleOverrideData} className="mt-6">
-        Override my data
-      </Button>
+      <section className="grid gap-6">
+        <div>
+          <label
+            htmlFor="selectFiles"
+            className="inline-flex items-center justify-center w-full px-6 py-3 text-base font-medium tracking-tight text-white transition duration-150 ease-in-out rounded-md cursor-pointer sm:w-auto focus:ring-2 focus:ring-offset-2 focus:outline-none hover:opacity-75 focus:opacity-75 bg-charcoal focus:ring-charcoal"
+          >
+            Choose file
+            <input
+              className="hidden"
+              type="file"
+              id="selectFiles"
+              onChange={handleFileChange}
+              accept="application/json"
+            />
+          </label>
+          <span className="ml-2">{overrideFilename || 'No file selected'}</span>
+        </div>
+        <section className="grid grid-cols-2 gap-4">
+          <Heading className="col-span-full">Override data details</Heading>
+          <InfoCard label="Account count">
+            {overrideAccountCount ? `${overrideAccountCount}` : '-'}
+          </InfoCard>
+          <InfoCard label="Transaction count">
+            {overrideTranactionCount ? `${overrideTranactionCount}` : '-'}
+          </InfoCard>
+        </section>
+        <div>
+          <Button onClick={handleOverrideData}>Override my data</Button>
+        </div>
+      </section>
     </>
   );
 };
