def512963d7204ae5cda2795a3804d01287e7eca
shamalainen <sebastian@hamse.fi>
2022-12-05 17:50:14 +0200

    Swaggerize health check

--- packages/backend/src/modules/health-check/health-check.controller.ts
+++ packages/backend/src/modules/health-check/health-check.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('health-check')
+@ApiTags('Health Check')
 export class HealthCheckController {
   @Get('ping')
+  @ApiOkResponse({ schema: { properties: { ping: { type: 'string' } } } })
   stuff() {
     return { ping: 'pong' };
   }
