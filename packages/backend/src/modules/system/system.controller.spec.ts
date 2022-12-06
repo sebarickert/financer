2642614de4e27f9ec9e3524c7a2a6ca9e31ec915
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-12-06 17:39:40 +0200

    Fix tests

--- packages/backend/src/modules/system/system.controller.spec.ts
+++ packages/backend/src/modules/system/system.controller.spec.ts
@@ -1,5 +1,9 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { rootMongooseTestModule } from '../../../test/rootMongooseTest.module';
+
+import { SystemLog, SystemLogSchema } from './schemas/system-log.schema';
 import { SystemController } from './system.controller';
 import { SystemService } from './system.service';
 
@@ -8,6 +12,12 @@ describe('SystemController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          { name: SystemLog.name, schema: SystemLogSchema },
+        ]),
+      ],
       controllers: [SystemController],
       providers: [SystemService],
     }).compile();
--- packages/backend/src/modules/system/system.service.spec.ts
+++ packages/backend/src/modules/system/system.service.spec.ts
@@ -1,5 +1,9 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { rootMongooseTestModule } from '../../../test/rootMongooseTest.module';
+
+import { SystemLog, SystemLogSchema } from './schemas/system-log.schema';
 import { SystemService } from './system.service';
 
 describe('SystemService', () => {
@@ -7,6 +11,12 @@ describe('SystemService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          { name: SystemLog.name, schema: SystemLogSchema },
+        ]),
+      ],
       providers: [SystemService],
     }).compile();
 
