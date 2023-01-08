c7168ca8fa06ae0c68e420c40746495fdd121760
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2023-01-08 16:31:10 +0200

    Refactor usage of edit account

--- packages/frontend/src/services/AccountService.ts
+++ packages/frontend/src/services/AccountService.ts
@@ -1,23 +1,3 @@
-import { AccountDto, ApiResponse, UpdateAccountDto } from '@local/types';
-
-import { parseApiResponse } from '../utils/apiHelper';
-
-export const editAccount = async (
-  id: string,
-  targetAccountData: UpdateAccountDto
-): Promise<ApiResponse<AccountDto>> => {
-  const targetAccount = await fetch(`/api/accounts/${id}`, {
-    method: 'PATCH',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(targetAccountData),
-  });
-
-  return parseApiResponse(targetAccount);
-};
-
 export const deleteAccount = async (id: string): Promise<void> => {
   await fetch(`/api/accounts/${id}`, {
     method: 'DELETE',
