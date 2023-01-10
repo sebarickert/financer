1437bd0c86a417f63e1dc2d259a978909332b084
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2023-01-10 23:05:58 +0200

    Refactor usage of Transfer endpoints

--- packages/frontend/src/pages/transfers/AddTransfer.tsx
+++ packages/frontend/src/pages/transfers/AddTransfer.tsx
@@ -4,19 +4,23 @@ import { useNavigate } from 'react-router-dom';
 
 import { TransferForm } from './TransferForm';
 
-import { CreateTransferDto } from '$api/generated/financerApi';
+import {
+  CreateTransferDto,
+  useTransfersCreateMutation,
+} from '$api/generated/financerApi';
 import { TransactionTemplateSwitcher } from '$blocks/transaction-template-switcher/transaction-template-switcher';
 import { Loader } from '$elements/loader/loader';
+import { LoaderFullScreen } from '$elements/loader/loader.fullscreen';
 import { useUserDefaultTransferSourceAccount } from '$hooks/profile/user-preference/useUserDefaultTransferSourceAccount';
 import { useUserDefaultTransferTargetAccount } from '$hooks/profile/user-preference/useUserDefaultTransferTargetAccount';
-import { useAddTransfer } from '$hooks/transfer/useAddTransfer';
 import { UpdatePageInfo } from '$renderers/seo/updatePageInfo';
 import { parseErrorMessagesToArray } from '$utils/apiHelper';
 
 export const AddTransfer = (): JSX.Element => {
   const navigate = useNavigate();
   const [errors, setErrors] = useState<string[]>([]);
-  const addTransaction = useAddTransfer();
+  const [addTransaction, { isLoading: isCreating }] =
+    useTransfersCreateMutation();
   const {
     data: defaultTransferSourceAccount,
     isLoading: isLoadingDefaultTransferSourceAccount,
@@ -28,11 +32,15 @@ export const AddTransfer = (): JSX.Element => {
 
   const handleSubmit = async (newTransfer: CreateTransferDto) => {
     try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const newTransactionJson = await addTransaction(newTransfer as any);
+      const newTransactionJson = await addTransaction({
+        createTransferDto: newTransfer,
+      });
 
       if ('message' in newTransactionJson) {
-        setErrors(parseErrorMessagesToArray(newTransactionJson.message));
+        setErrors(
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          parseErrorMessagesToArray((newTransactionJson as any).message)
+        );
         return;
       }
 
@@ -49,6 +57,7 @@ export const AddTransfer = (): JSX.Element => {
 
   return (
     <>
+      {isCreating && <LoaderFullScreen />}
       <UpdatePageInfo
         title="Add transfer"
         headerAction={
--- packages/frontend/src/pages/transfers/Transfer.tsx
+++ packages/frontend/src/pages/transfers/Transfer.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import { useAccountsFindOneByIdQuery } from '$api/generated/financerApi';
+import {
+  TransactionCategoryMappingDto,
+  useAccountsFindOneByIdQuery,
+  useTransfersFindOneQuery,
+  useTransfersRemoveMutation,
+} from '$api/generated/financerApi';
+import { DataHandler } from '$blocks/data-handler/data-handler';
 import { Button } from '$elements/button/button';
 import { ButtonGroup } from '$elements/button/button.group';
 import { DialogConfirm } from '$elements/dialog/confirm/dialog.confirm';
@@ -9,9 +15,8 @@ import { Dialog } from '$elements/dialog/dialog';
 import { Divider } from '$elements/divider/divider';
 import { IconName } from '$elements/icon/icon';
 import { InfoCard } from '$elements/info-card/info-card';
+import { LoaderFullScreen } from '$elements/loader/loader.fullscreen';
 import { useAllTransactionCategoriesWithCategoryTree } from '$hooks/transactionCategories/useAllTransactionCategories';
-import { useDeleteTransfer } from '$hooks/transfer/useDeleteTransfer';
-import { useTransferById } from '$hooks/transfer/useTransferById';
 import { UpdatePageInfo } from '$renderers/seo/updatePageInfo';
 import { formatCurrency } from '$utils/formatCurrency';
 import { formatDate } from '$utils/formatDate';
@@ -50,16 +55,26 @@ const TransferDeleteModal = ({ handleDelete }: ITransferDeleteModalProps) => {
 
 export const Transfer = (): JSX.Element => {
   const navigate = useNavigate();
-  const { id } = useParams<{ id: string }>();
+  const { id = 'id-missing' } = useParams<{ id: string }>();
   const transactionCategories = useAllTransactionCategoriesWithCategoryTree();
-  const transfer = useTransferById(id);
-  const deleteTransfer = useDeleteTransfer();
+  const transferData = useTransfersFindOneQuery({ id });
+  const { data: transfer } = transferData;
 
-  const fromAccountData = useAccountsFindOneByIdQuery({
-    id: transfer.fromAccount,
-  });
+  const [deleteTransfer, { isLoading: isDeleting }] =
+    useTransfersRemoveMutation();
+
+  const fromAccountData = useAccountsFindOneByIdQuery(
+    {
+      id: transfer?.fromAccount as string,
+    },
+    { skip: !transfer }
+  );
+
   const fromAccount = fromAccountData.data;
-  const toAccountData = useAccountsFindOneByIdQuery({ id: transfer.toAccount });
+  const toAccountData = useAccountsFindOneByIdQuery(
+    { id: transfer?.toAccount as string },
+    { skip: !transfer }
+  );
   const toAccount = toAccountData.data;
 
   const getCategoryNameById = (categoryId: string) =>
@@ -71,86 +86,92 @@ export const Transfer = (): JSX.Element => {
       console.error('Failed to delete transfer: no id');
       return;
     }
-    await deleteTransfer(id);
+    await deleteTransfer({ id }).unwrap();
     navigate('/statistics/transfers');
   };
 
   return (
     <>
+      {isDeleting && <LoaderFullScreen />}
+      <DataHandler {...transferData} />
       <UpdatePageInfo
-        title={`${transfer.description}`}
+        title={`${transfer?.description}`}
         backLink="/statistics/transfers"
       />
-      <section className="grid gap-2 lg:gap-4">
-        <section className="grid grid-cols-2 gap-2 lg:grid-cols-3 md:gap-4">
-          <InfoCard iconName={IconName.cash} label="Amount">
-            {formatCurrency(transfer.amount)}
-          </InfoCard>
-          <InfoCard iconName={IconName.calendar} label="Date">
-            {formatDate(new Date(transfer.date))}
-          </InfoCard>
-          <InfoCard
-            iconName={IconName.informationCircle}
-            label="Type"
-            className="max-lg:col-span-full"
-            isLarge
-          >
-            Transfer
-          </InfoCard>
+      {transfer && (
+        <section className="grid gap-2 lg:gap-4">
+          <section className="grid grid-cols-2 gap-2 lg:grid-cols-3 md:gap-4">
+            <InfoCard iconName={IconName.cash} label="Amount">
+              {formatCurrency(transfer.amount)}
+            </InfoCard>
+            <InfoCard iconName={IconName.calendar} label="Date">
+              {formatDate(new Date(transfer.date))}
+            </InfoCard>
+            <InfoCard
+              iconName={IconName.informationCircle}
+              label="Type"
+              className="max-lg:col-span-full"
+              isLarge
+            >
+              Transfer
+            </InfoCard>
+          </section>
+          <section className="grid grid-cols-2 gap-2 lg:gap-4">
+            <InfoCard
+              iconName={IconName.upload}
+              label="From account"
+              className="max-lg:col-span-full"
+              isLarge
+            >
+              {fromAccount?.name ?? '-'}
+            </InfoCard>
+            <InfoCard
+              iconName={IconName.download}
+              label="To account"
+              className="max-lg:col-span-full"
+              isLarge
+            >
+              {toAccount?.name ?? '-'}
+            </InfoCard>
+          </section>
+          {transfer.categories.length > 0 && (
+            <>
+              <Divider>Categories</Divider>
+              <ul className="grid gap-2 lg:gap-4">
+                {(
+                  transfer.categories as unknown as TransactionCategoryMappingDto[]
+                )?.map(({ amount, category_id }) => (
+                  <li className="grid grid-cols-2 gap-2 lg:gap-4">
+                    <InfoCard
+                      iconName={IconName.tag}
+                      label="Category"
+                      testId="category_label"
+                    >
+                      {getCategoryNameById(category_id as unknown as string)}
+                    </InfoCard>
+                    <InfoCard
+                      iconName={IconName.cash}
+                      label="Amount"
+                      testId="category_amount"
+                    >
+                      {formatCurrency(amount)}
+                    </InfoCard>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+          <ButtonGroup className="mt-4">
+            <Button
+              link={`/statistics/transfers/${id}/edit`}
+              testId="edit-transfer-button"
+            >
+              Edit
+            </Button>
+            <TransferDeleteModal handleDelete={handleDelete} />
+          </ButtonGroup>
         </section>
-        <section className="grid grid-cols-2 gap-2 lg:gap-4">
-          <InfoCard
-            iconName={IconName.upload}
-            label="From account"
-            className="max-lg:col-span-full"
-            isLarge
-          >
-            {fromAccount?.name ?? '-'}
-          </InfoCard>
-          <InfoCard
-            iconName={IconName.download}
-            label="To account"
-            className="max-lg:col-span-full"
-            isLarge
-          >
-            {toAccount?.name ?? '-'}
-          </InfoCard>
-        </section>
-        {transfer.categories.length > 0 && (
-          <>
-            <Divider>Categories</Divider>
-            <ul className="grid gap-2 lg:gap-4">
-              {transfer.categories?.map(({ amount, category_id }) => (
-                <li className="grid grid-cols-2 gap-2 lg:gap-4">
-                  <InfoCard
-                    iconName={IconName.tag}
-                    label="Category"
-                    testId="category_label"
-                  >
-                    {getCategoryNameById(category_id)}
-                  </InfoCard>
-                  <InfoCard
-                    iconName={IconName.cash}
-                    label="Amount"
-                    testId="category_amount"
-                  >
-                    {formatCurrency(amount)}
-                  </InfoCard>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-        <ButtonGroup className="mt-4">
-          <Button
-            link={`/statistics/transfers/${id}/edit`}
-            testId="edit-transfer-button"
-          >
-            Edit
-          </Button>
-          <TransferDeleteModal handleDelete={handleDelete} />
-        </ButtonGroup>
-      </section>
+      )}
     </>
   );
 };
