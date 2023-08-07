51b9498fbf5e5e9bbea2a82c97c3ea5726e54ae3
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2023-08-07 22:32:22 +0300

    Add sw to nextjs

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -23,4 +23,23 @@ const nextConfig = {
 };
 
 
-module.exports = nextConfig; 
+/**
+ * @type {import('next-pwa').PWAConfig}
+ */
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: isDevelopment,
+    dynamicStartUrl: true,
+    sw: "/service-worker.js", 
+    runtimeCaching: [
+        {
+            urlPattern: ({url}) => url.pathname.startsWith("/api") || url.pathname.startsWith("/auth"),
+            handler: "NetworkOnly",
+        },
+    ]
+  });
+
+
+module.exports = withPWA(nextConfig); 
