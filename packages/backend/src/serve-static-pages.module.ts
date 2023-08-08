e058ec8f4a7c6c0b7d975168e700e8bfca5f550c
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2023-08-08 16:18:33 +0300

    Fix static pages

--- packages/backend/src/serve-static-pages.module.ts
+++ packages/backend/src/serve-static-pages.module.ts
@@ -5,9 +5,24 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    // Accounts dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/accounts/add'),
+      serveRoot: '/accounts/add',
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/accounts/[accountId]'),
       serveRoot: '/accounts/:id',
+    }),
+
+    // Transaction categories dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(
+        __dirname,
+        '..',
+        'client/profile/transaction-categories/add',
+      ),
+      serveRoot: '/profile/transaction-categories/add',
     }),
     ServeStaticModule.forRoot({
       rootPath: join(
@@ -17,6 +32,16 @@ import { ServeStaticModule } from '@nestjs/serve-static';
       ),
       serveRoot: '/profile/transaction-categories/:id',
     }),
+
+    // Transaction templates dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(
+        __dirname,
+        '..',
+        'client/profile/transaction-templates/add',
+      ),
+      serveRoot: '/profile/transaction-templates/add',
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(
         __dirname,
@@ -25,13 +50,31 @@ import { ServeStaticModule } from '@nestjs/serve-static';
       ),
       serveRoot: '/profile/transaction-templates/:id',
     }),
+
+    // Expense dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/statistics/expenses/add'),
+      serveRoot: '/statistics/expenses/add',
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/statistics/expenses/[expenseId]'),
       serveRoot: '/statistics/expenses/:id',
     }),
+
+    // Income dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/statistics/incomes/add'),
+      serveRoot: '/statistics/incomes/add',
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/statistics/incomes/[incomeId]'),
       serveRoot: '/statistics/incomes/:id',
+    }),
+
+    // Transfer dynamic paths
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/statistics/transfers/add'),
+      serveRoot: '/statistics/transfers/add',
     }),
     ServeStaticModule.forRoot({
       rootPath: join(
@@ -41,6 +84,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
       ),
       serveRoot: '/statistics/transfers/:id',
     }),
+
+    // Serve static files
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       exclude: ['/health-check/*', '/api/*', '/auth/*'],
