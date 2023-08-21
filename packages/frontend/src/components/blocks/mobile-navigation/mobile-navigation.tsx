1875a1299826762d09df825df344819f06762de2
shamalainen <sebastian@hamse.fi>
2023-08-21 18:17:51 +0300

    WIp

--- packages/frontend/src/components/blocks/mobile-navigation/mobile-navigation.tsx
+++ packages/frontend/src/components/blocks/mobile-navigation/mobile-navigation.tsx
@@ -5,7 +5,7 @@ import { navigationItems } from '$constants/navigation-items';
 
 export const MobileNavigation = (): JSX.Element => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-10 w-full border-t bg-gray border-gray-dark pb-safe vt-name-[mobile-navigation]">
+    <div className="fixed bottom-0 left-0 right-0 w-full border-t bg-gray border-gray-dark pb-safe vt-name-[mobile-navigation]">
       <nav>
         <ul className={`grid grid-cols-5 relative`}>
           <MobileNavigationItem {...navigationItems.home} isExact />
