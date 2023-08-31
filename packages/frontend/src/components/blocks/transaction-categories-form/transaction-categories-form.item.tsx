ff9f746a8e50ce415a8aaf0d187eb05f9d09247a
shamalainen <sebastian@hamse.fi>
2023-08-31 20:57:59 +0300

    transaction category shenanigangs
    Co-authored-by: Teemu Sillantaus <silte@users.noreply.github.com>

--- packages/frontend/src/components/blocks/transaction-categories-form/transaction-categories-form.item.tsx
+++ packages/frontend/src/components/blocks/transaction-categories-form/transaction-categories-form.item.tsx
@@ -1,9 +1,15 @@
+import { UseFieldArrayRemove, useFormContext } from 'react-hook-form';
+
 import { Button } from '../../elements/button/button';
 import { Divider } from '../../elements/divider/divider';
 import { Icon, IconName } from '../../elements/icon/icon';
 import { Input } from '../../elements/input/input';
 import { Option } from '../../elements/select/select';
 import { Select } from '../../elements/select/select';
+
+import { FieldArrayFields } from './transaction-categories-form';
+
+import { ButtonGroup } from '$elements/button/button.group';
 
 interface TransactionCategoriesFormItemProps {
   className?: string;
@@ -13,8 +19,9 @@ interface TransactionCategoriesFormItemProps {
   setUnallocatedAmount(): void;
   testId?: string;
   categorySelectOnly?: boolean;
-  namePrefix: string;
-  maxAmount: number;
+  // maxAmount: number;
+  remove: UseFieldArrayRemove;
+  onClose: () => void;
 }
 
 export const TransactionCategoriesFormItem = ({
@@ -25,63 +32,92 @@ export const TransactionCategoriesFormItem = ({
   setUnallocatedAmount,
   testId = '',
   categorySelectOnly,
-  namePrefix,
-  maxAmount,
+  // maxAmount,
+  remove,
+  onClose,
 }: TransactionCategoriesFormItemProps): JSX.Element => {
+  const namePrefix = `categories.${index}` as const;
+
+  const { getValues } = useFormContext<FieldArrayFields>();
+
+  const handleSubmit = () => {
+    onClose();
+  };
+
   return (
     <div
       className={className}
       data-testid={`${testId}_transaction-category_row`}
     >
       <Divider>{`Category Item #${index + 1}`}</Divider>
-      <div className="grid sm:grid-cols-[1fr,auto] gap-6 sm:gap-4 items-start mt-4">
-        <div className="space-y-4">
-          <div className="grid gap-4">
-            <Select
-              id={`${namePrefix}.category_id`}
-              options={categories}
-              isRequired
-              testId={`${testId}_transaction-category_category`}
-            >
-              Category
-            </Select>
-            {!categorySelectOnly && (
-              <>
-                <div className="grid grid-cols-[1fr,auto] gap-2 items-end">
-                  <Input
-                    id={`${namePrefix}.amount`}
-                    type="number"
-                    min={0.01}
-                    step={0.01}
-                    max={maxAmount}
-                    isCurrency
-                    isRequired
-                    testId={`${testId}_transaction-category_amount`}
-                  >
-                    Amount
-                  </Input>
-                  <Button onClick={setUnallocatedAmount} accentColor="plain">
-                    <span className="sr-only">Add unallocated amount</span>
-                    <Icon type={IconName.plusCircle} />
-                  </Button>
-                </div>
+      <div className="grid sm:grid-cols-[1fr,auto] items-start mt-4">
+        <div className="grid gap-4">
+          <Select
+            id={`${namePrefix}.category_id`}
+            options={categories}
+            isRequired
+            testId={`${testId}_transaction-category_category`}
+          >
+            Category
+          </Select>
+          {!categorySelectOnly && (
+            <>
+              <div className="grid grid-cols-[1fr,auto] gap-2 items-end">
                 <Input
-                  id={`${namePrefix}.description`}
-                  testId={`${testId}_transaction-category_description`}
+                  id={`${namePrefix}.amount`}
+                  type="number"
+                  min={0.01}
+                  step={0.01}
+                  // max={maxAmount}
+                  isCurrency
+                  isRequired
+                  testId={`${testId}_transaction-category_amount`}
                 >
-                  Description
+                  Amount
                 </Input>
-              </>
-            )}
-          </div>
+                <button
+                  onClick={setUnallocatedAmount}
+                  className="inline-flex items-center justify-center h-[50px] w-11 focus:ring-black focus:border-black focus:outline-black rounded-md"
+                >
+                  <span className="sr-only">Add unallocated amount</span>
+                  <Icon type={IconName.plus} />
+                </button>
+                {/* <Button onClick={setUnallocatedAmount} accentColor="plain"> */}
+                {/* </Button> */}
+              </div>
+              <Input
+                id={`${namePrefix}.description`}
+                testId={`${testId}_transaction-category_description`}
+              >
+                Description
+              </Input>
+            </>
+          )}
         </div>
-        <Button
+        <ButtonGroup className="mt-12" isReverse isHorizontal>
+          <Button
+            className="sm:mt-6"
+            onClick={handleSubmit}
+            testId={`${testId}_delete-button`}
+          >
+            Add category
+          </Button>
+          <Button
+            className="sm:mt-6"
+            // onClick={deleteTransactionCategoryItem}
+            testId={`${testId}_delete-button`}
+            accentColor="plain"
+          >
+            Cancel
+          </Button>
+        </ButtonGroup>
+        {/* <Button
           className="sm:mt-6"
           onClick={deleteTransactionCategoryItem}
           testId={`${testId}_delete-button`}
         >
           Delete
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
--- packages/frontend/src/components/blocks/transaction-categories-form/transaction-categories-form.tsx
+++ packages/frontend/src/components/blocks/transaction-categories-form/transaction-categories-form.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { useFieldArray, useFormContext, useWatch } from 'react-hook-form';
 
 import { TransactionCategoriesFormItem } from './transaction-categories-form.item';
 
+import { Drawer } from '$blocks/drawer/drawer';
 import { Button } from '$elements/button/button';
 import { Option } from '$elements/select/select';
 
@@ -18,9 +20,34 @@ export interface TransactionCategoriesFormFields {
   amount: number;
 }
 
-interface FieldArrayFields {
+export interface FieldArrayFields {
   categories: TransactionCategoriesFormFields[];
 }
+
+type PlaaProps = {
+  index: number;
+  onClick: () => void;
+};
+
+const Plaa = ({ index, onClick }: PlaaProps) => {
+  const { watch, getValues } = useFormContext<FieldArrayFields>();
+
+  const values = watch(`categories.${index}`);
+
+  const isEmptyCategory = isNaN(parseInt(`${values.amount}`));
+
+  if (isEmptyCategory) return null;
+
+  return (
+    <li className="flex flex-col">
+      <b>
+        {getValues(`categories.${index}.description`)} -{' '}
+        {getValues(`categories.${index}.amount`)}
+      </b>
+      <button onClick={onClick}>CLICK MEEEEE</button>
+    </li>
+  );
+};
 
 export const TransactionCategoriesForm = ({
   className = '',
@@ -31,18 +58,88 @@ export const TransactionCategoriesForm = ({
   const { fields, append, remove } = useFieldArray<FieldArrayFields>({
     name: 'categories',
   });
+  const { getValues } = useFormContext<FieldArrayFields>();
+  const [selectedIndex, setSelectedIndex] = useState(NaN);
+  const [previousSelected, setPreviousSelected] = useState(NaN);
 
-  const { setValue } = useFormContext();
+  // const { setValue } = useFormContext();
 
-  const transactionAmount = useWatch({ name: 'amount' });
+  // const transactionAmount = useWatch({ name: 'amount' });
 
   const totalAllocatedAmount = fields
     .map(({ amount }) => amount || 0)
     .reduce((current, previous) => current + previous, 0);
 
+  console.log(fields);
+
+  const addNewCategory = () => {
+    append({} as TransactionCategoriesFormFields);
+    setSelectedIndex(fields.length);
+  };
+
+  const isEmptyCategory = (index: number) =>
+    isNaN(parseInt(`${getValues(`categories.${index}`).amount}`));
+
+  const onClose = () => {
+    const values = getValues(`categories.${selectedIndex}`);
+
+    console.log('ASDASDASD', parseInt(`${values.amount}`), selectedIndex);
+    if (isEmptyCategory(selectedIndex)) {
+      remove(selectedIndex);
+    }
+    setPreviousSelected(selectedIndex);
+    setSelectedIndex(NaN);
+  };
+
   return (
     <>
-      <div className={className} data-testid={testId}>
+      <button
+        onClick={() => console.log(getValues(`categories.${previousSelected}`))}
+      >
+        PLAAA PLAAA
+      </button>
+
+      <Button
+        // onClick={() => append({} as TransactionCategoriesFormFields)}
+        onClick={addNewCategory}
+        accentColor="plain"
+        // isDisabled={!transactionAmount || transactionAmount < 0}
+        testId="add-category-button"
+      >
+        Add category
+      </Button>
+      <Drawer
+        isOpen={!isNaN(selectedIndex)}
+        onClose={onClose}
+        heading="Add category item"
+      >
+        <TransactionCategoriesFormItem
+          key={selectedIndex}
+          index={selectedIndex}
+          categories={transactionCategories}
+          deleteTransactionCategoryItem={() => remove(selectedIndex)}
+          setUnallocatedAmount={() => {
+            // const unallocatedAmount =
+            //   transactionAmount - totalAllocatedAmount + (0 || 0);
+            // setValue(`categories.${selectedIndex}.amount`, unallocatedAmount);
+          }}
+          testId={testId}
+          categorySelectOnly={categorySelectOnly}
+          // maxAmount={transactionAmount}
+          remove={remove}
+          onClose={onClose}
+        />
+      </Drawer>
+      <ul>
+        {fields.map(({ id, amount = 0 }, index) => (
+          <Plaa
+            key={id}
+            index={index}
+            onClick={() => setSelectedIndex(index)}
+          />
+        ))}
+      </ul>
+      {/* <div className={className} data-testid={testId}>
         {fields.map(({ id, amount = 0 }, index) => (
           <TransactionCategoriesFormItem
             index={index}
@@ -60,15 +157,7 @@ export const TransactionCategoriesForm = ({
             maxAmount={transactionAmount}
           />
         ))}
-      </div>
-      <Button
-        onClick={() => append({} as TransactionCategoriesFormFields)}
-        accentColor="plain"
-        isDisabled={!transactionAmount || transactionAmount < 0}
-        testId="add-category-button"
-      >
-        Add category item
-      </Button>
+      </div> */}
     </>
   );
 };
