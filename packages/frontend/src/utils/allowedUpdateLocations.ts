95bc0590d73063c38989b8dcf31f8df8c9bd365b
shamalainen <sebastian@hamse.fi>
2023-12-08 13:34:52 +0200

    Delete dead code

--- packages/frontend/src/utils/allowedUpdateLocations.ts
+++
@@ -1,6 +0,0 @@
-export const isUpdateAllowed = (location: string): boolean => {
-  const isEditPage = location.includes('/edit');
-  const isAddPage = location.includes('/add');
-
-  return !isEditPage && !isAddPage;
-};
