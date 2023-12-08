9c55d36884fd3fdfdfc040e7fc3767eacddf3264
shamalainen <sebastian@hamse.fi>
2023-12-08 14:36:46 +0200

    Delete infoCard component

--- packages/frontend/src/pages/settings/override-user-data.tsx
+++ packages/frontend/src/pages/settings/override-user-data.tsx
@@ -1,9 +1,10 @@
 import { ChangeEvent } from 'react';
 
+import { DetailsList } from '$blocks/details-list/details-list';
 import { settingsPaths } from '$constants/settings-paths';
 import { Button } from '$elements/button/button';
 import { Heading } from '$elements/heading/heading';
-import { InfoCard } from '$elements/info-card/info-card';
+import { IconName } from '$elements/icon/icon';
 import { LoaderFullScreen } from '$elements/loader/loader.fullscreen';
 import { UpdatePageInfo } from '$renderers/seo/updatePageInfo';
 
@@ -24,6 +25,19 @@ export const OverrideUserData = ({
   onFileChange,
   onOverrideData,
 }: OverrideUserDataProps): JSX.Element => {
+  const overrideDetails = [
+    {
+      icon: IconName.informationCircle,
+      label: 'Account count',
+      description: overrideAccountCount ?? '-',
+    },
+    {
+      icon: IconName.informationCircle,
+      label: 'Transaction count',
+      description: overrideTranactionCount ?? '-',
+    },
+  ];
+
   return (
     <>
       {isLoading && <LoaderFullScreen />}
@@ -31,36 +45,29 @@ export const OverrideUserData = ({
         title="Override data (DANGER ZONE)"
         backLink={settingsPaths.default}
       />
-      <section className="grid gap-6">
-        <div>
-          <label
-            htmlFor="selectFiles"
-            className="inline-flex items-center justify-center w-full px-6 py-3 text-base font-medium tracking-tight text-white transition duration-150 ease-in-out rounded-md cursor-pointer sm:w-auto focus:ring-2 focus:ring-offset-2 focus:outline-none hover:opacity-75 focus:opacity-75 bg-charcoal focus:ring-charcoal"
-          >
-            Choose file
-            <input
-              className="hidden"
-              type="file"
-              id="selectFiles"
-              onChange={onFileChange}
-              accept="application/json"
-            />
-          </label>
-          <span className="ml-2">{overrideFilename || 'No file selected'}</span>
-        </div>
-        <section className="grid grid-cols-2 gap-4">
-          <Heading className="col-span-full">Override data details</Heading>
-          <InfoCard label="Account count">
-            {overrideAccountCount ? `${overrideAccountCount}` : '-'}
-          </InfoCard>
-          <InfoCard label="Transaction count">
-            {overrideTranactionCount ? `${overrideTranactionCount}` : '-'}
-          </InfoCard>
-        </section>
-        <div>
-          <Button onClick={onOverrideData}>Override my data</Button>
-        </div>
+      <div className="mb-8">
+        <label
+          htmlFor="selectFiles"
+          className="inline-flex items-center justify-center w-full px-6 py-3 text-base font-medium tracking-tight text-white transition duration-150 ease-in-out rounded-md cursor-pointer sm:w-auto focus:ring-2 focus:ring-offset-2 focus:outline-none hover:opacity-75 focus:opacity-75 bg-charcoal focus:ring-charcoal"
+        >
+          Choose file
+          <input
+            className="hidden"
+            type="file"
+            id="selectFiles"
+            onChange={onFileChange}
+            accept="application/json"
+          />
+        </label>
+        <span className="ml-2">{overrideFilename || 'No file selected'}</span>
+      </div>
+      <section>
+        <Heading>Override data details</Heading>
+        <DetailsList items={overrideDetails} className="mt-4" />
       </section>
+      <Button onClick={onOverrideData} className="mt-12">
+        Override my data
+      </Button>
     </>
   );
 };
