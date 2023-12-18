932eff127c548a34dae14f9bda3192a0e36e428e
shamalainen <sebastian@hamse.fi>
2023-12-18 18:26:50 +0200

    Fix loader

--- packages/frontend/src/components/elements/loader/loader.tsx
+++ packages/frontend/src/components/elements/loader/loader.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { LoaderFullScreen } from './loader.fullscreen';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import logo from '$assets/logo.svg';
@@ -9,26 +11,27 @@ type LoaderProps = {
   children?: React.ReactNode;
 };
 
+const LoaderIcon = () => (
+  <div className="flex items-center justify-center p-16">
+    <Image
+      src={logo}
+      className="absolute block w-12 h-12 opacity-75 animate-ping"
+      alt="logo"
+    />
+    <Image src={logo} className="relative block rounded h-14 w-14" alt="logo" />
+  </div>
+);
+
 export const Loader = ({
   children = null,
   isLoading = true,
 }: LoaderProps): JSX.Element => {
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center p-16">
-        <Image
-          src={logo}
-          className="absolute block w-12 h-12 opacity-75 animate-ping"
-          alt="logo"
-        />
-        <Image
-          src={logo}
-          className="relative block rounded h-14 w-14"
-          alt="logo"
-        />
-      </div>
-    );
-  }
-
-  return <>{children}</>;
+  return (
+    <>
+      {isLoading && <LoaderFullScreen />}
+      {children}
+    </>
+  );
 };
+
+Loader.Icon = LoaderIcon;
