b58e4b9c8052c3ecab64924510021a8ff8534f41
shamalainen <sebastian@hamse.fi>
2023-12-18 16:28:11 +0200

    Update chart styles

---
+++ packages/frontend/src/constants/graph/graph.settings.ts
@@ -0,0 +1,115 @@
+import { ChartOptions } from 'chart.js';
+
+import { colorPalette } from '$constants/colorPalette';
+import {
+  formatCurrencyAbbreviation,
+  formatCurrency,
+} from '$utils/formatCurrency';
+
+export const baseChartFontFamily = 'Euclid Circular A';
+
+export const baseChartOptions = {
+  animation: false,
+  maintainAspectRatio: false,
+  interaction: {
+    intersect: false,
+    mode: 'index',
+  },
+  layout: {
+    autoPadding: false,
+    padding: {
+      right: -10,
+    },
+  },
+  scales: {
+    x: {
+      border: {
+        display: false,
+      },
+      grid: {
+        display: false,
+      },
+      ticks: {
+        padding: 0,
+        maxRotation: 0,
+        color: `${colorPalette.charcoal}99`,
+        font: {
+          size: 12,
+          family: baseChartFontFamily,
+          weight: 'normal',
+        },
+      },
+    },
+    y: {
+      position: 'right',
+      border: {
+        display: false,
+      },
+      grid: {
+        color: `${colorPalette.charcoal}0D`,
+      },
+      ticks: {
+        mirror: true,
+        padding: 0,
+        callback: function (val, index, ticks) {
+          if (index % 2 === 0 || ticks.length - 1 === index) return null;
+
+          return `${formatCurrencyAbbreviation(Number(val))} `;
+        },
+        color: `${colorPalette.charcoal}66`,
+      },
+    },
+  },
+  elements: {
+    point: {
+      hitRadius: 32,
+      radius: 0,
+      hoverBorderWidth: 3,
+      hoverRadius: 3,
+    },
+    line: {
+      borderWidth: 2,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    filler: {
+      propagate: true,
+    },
+    tooltip: {
+      backgroundColor: colorPalette.charcoal,
+      padding: 16,
+      bodySpacing: 6,
+      displayColors: false,
+      titleFont: {
+        size: 15,
+        family: baseChartFontFamily,
+      },
+      bodyFont: {
+        size: 15,
+        family: baseChartFontFamily,
+        weight: 'normal',
+      },
+      footerFont: {
+        size: 15,
+        family: baseChartFontFamily,
+        weight: 'normal',
+      },
+      callbacks: {
+        label: (context) => {
+          const label = context.dataset.label || '';
+
+          if (!context.parsed.y) {
+            return `${label} ${formatCurrency(0)}`.toUpperCase();
+          }
+
+          return `${label} ${formatCurrency(
+            context.parsed.y as number
+          )}`.toUpperCase();
+        },
+      },
+    },
+  },
+} as ChartOptions;
--- packages/frontend/src/utils/graph/setGradientLineGraphBackground.ts
+++ packages/frontend/src/utils/graph/setGradientLineGraphBackground.ts
@@ -2,6 +2,19 @@ import { ScriptableContext } from 'chart.js';
 
 import { colorPalette } from '$constants/colorPalette';
 
+const getGradientColorByLabel = (label: string) => {
+  switch (label) {
+    case 'expenses':
+      return colorPalette.red;
+
+    case 'incomes':
+      return colorPalette.green;
+
+    default:
+      return colorPalette.blue;
+  }
+};
+
 export const setGradientLineGraphBackground = (
   context: ScriptableContext<'line'>
 ) => {
@@ -9,10 +22,14 @@ export const setGradientLineGraphBackground = (
 
   if (!chartArea) return;
 
+  const label = context.dataset.label;
+
   const { top, bottom } = chartArea;
   const gradient = ctx.createLinearGradient(0, bottom, 0, top);
 
-  gradient.addColorStop(0.5, `${colorPalette.blue}1A`);
+  const color = getGradientColorByLabel(label?.toLowerCase() || '');
+
+  gradient.addColorStop(0.5, `${color}1A`);
   gradient.addColorStop(0, 'transparent');
 
   return gradient;
