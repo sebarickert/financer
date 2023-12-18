e442a624b9d977442fab4d2499777e5d2700a8d9
shamalainen <sebastian@hamse.fi>
2023-12-18 18:51:42 +0200

    Update loader

--- packages/frontend/src/components/elements/loader/loader.fullscreen.tsx
+++ packages/frontend/src/components/elements/loader/loader.fullscreen.tsx
@@ -3,9 +3,9 @@ import { Loader } from './loader';
 export const LoaderFullScreen = (): JSX.Element => (
   <div className="fixed inset-0 z-50 flex items-center w-full h-full">
     <div className="fixed inset-0 transition-opacity">
-      <div className="absolute inset-0 opacity-50 bg-gray" />
+      <div className="absolute inset-0 opacity-80 bg-white" />
     </div>
-    <div className="w-full">
+    <div className="w-full flex items-center justify-center">
       <Loader.Icon />
     </div>
   </div>
