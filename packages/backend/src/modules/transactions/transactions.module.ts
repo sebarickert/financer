50c0521ac86d68746d79d7f2d327f48ad6e6c0e5
shamalainen <sebastian@hamse.fi>
2023-12-28 18:16:36 +0200

    Fix errors

--- packages/backend/src/modules/transactions/transactions.module.ts
+++ packages/backend/src/modules/transactions/transactions.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
+import { AccountBalanceChangesService } from '../account-balance-changes/account-balance-changes.service';
+import { AccountsService } from '../accounts/accounts.service';
+import { TransactionCategoriesService } from '../transaction-categories/transaction-categories.service';
+import { TransactionCategoryMappingsService } from '../transaction-category-mappings/transaction-category-mappings.service';
 
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
@@ -8,7 +12,13 @@ import { TransactionsService } from './transactions.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [
+    TransactionsService,
+    AccountsService,
+    TransactionCategoriesService,
+    TransactionCategoryMappingsService,
+    AccountBalanceChangesService,
+  ],
   exports: [TransactionsService],
 })
 export class TransactionsModule {}
