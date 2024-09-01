88698d6d0049991a916a1f911ba45ecf42aa2266
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2024-09-01 17:58:37 +0300

    Fix dashboard

--- packages/frontend/src/views/dashboard/dashboard.tsx
+++ packages/frontend/src/views/dashboard/dashboard.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { BalanceGraph } from '$blocks/balance-graph/balance-graph';
+import { DashboardStats } from '$blocks/dashboard-stats/dashboard.stats';
 import { TransactionListingContainer } from '$blocks/transaction-listing/transaction-listing.container';
 import { Heading } from '$elements/heading/heading';
 import { UpdatePageInfo } from '$renderers/seo/updatePageInfo';
@@ -7,10 +11,10 @@ export const Dashboard = (): JSX.Element => {
     <>
       <UpdatePageInfo title="Dashboard" />
       <section className="grid gap-6 md:gap-8">
-        {/* <section>
+        <section>
           <DashboardStats />
           <BalanceGraph />
-        </section> */}
+        </section>
         <section>
           <Heading className="mb-4" ctaLabel="See all" ctaUrl="/statistics">
             Recent activity
