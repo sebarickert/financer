e2ddf0eb37368987e28f944e45152374274ac90b
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2024-10-31 17:10:03 +0200

    Fix e2e test CI reporting

--- packages/playwright/playwright.config.ts
+++ packages/playwright/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   workers: getWorkerCount(),
   // workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "list" : [['list'], ['html']],
+  reporter: process.env.CI ? [['list'], ['blob']] : [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
