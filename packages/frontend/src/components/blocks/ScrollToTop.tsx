20bf87a1c5d4af1a24466b0195c2a429f5328b75
sebarickert <sebastian@serk.fi>
2024-10-31 09:12:03 +0200

    Revert back ScrollToTop fix

---
+++ packages/frontend/src/components/blocks/ScrollToTop.tsx
@@ -0,0 +1,12 @@
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+export const ScrollToTop = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
