2ff08b1c0603ad3eeb3fcabade45f795618eebc0
sebarickert <sebastian@serk.fi>
2024-11-17 12:53:51 +0200

    Add page Layout for error pages

--- packages/frontend/app/(application)/(error)/error.tsx
+++ packages/frontend/app/(application)/(error)/error.tsx
@@ -3,12 +3,17 @@
 import { Metadata } from 'next';
 import { FC } from 'react';
 
+import { Layout } from '$layouts/Layout';
 import { Error as ErrorPageComponent } from '$views/Error';
 
 export const metadata: Metadata = {
   title: 'Error',
 };
 
-const ErrorPage: FC = () => <ErrorPageComponent errorPageType="in-app" />;
+const ErrorPage: FC = () => (
+  <Layout title="Error">
+    <ErrorPageComponent errorPageType="in-app" />
+  </Layout>
+);
 
 export default ErrorPage;
