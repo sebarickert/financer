25917f67fabc61355eea8e40584e788b81e1a989
sebarickert <sebastian@serk.fi>
2024-12-01 11:33:57 +0200

    Update function

--- packages/playwright/utils/applyFixture.ts
+++ packages/playwright/utils/applyFixture.ts
@@ -25,7 +25,7 @@ const updateTransactionDates = (
 
   // Calculate the day offset to make the newest date equal to today's date
   const currentDate = new Date();
-  const dayOffset = Math.floor(
+  const dayOffset = Math.ceil(
     (currentDate.getTime() - newestDate.getTime()) / (1000 * 60 * 60 * 24),
   );
 
