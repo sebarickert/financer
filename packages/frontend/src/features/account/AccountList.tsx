1a660435d3acb6c26393772a19f6f93da424feae
sebarickert <sebastian@serk.fi>
2024-12-19 11:36:21 +0200

    Enhance account type mapping to include color property and update AccountList component for dynamic styling

--- packages/frontend/src/features/account/AccountList.tsx
+++ packages/frontend/src/features/account/AccountList.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 
-import { AccountDto, AccountType } from '$api/generated/financerApi';
+import { AccountDto } from '$api/generated/financerApi';
 import { BalanceDisplay } from '$blocks/BalanceDisplay';
 import { List } from '$blocks/List';
 import { ACCOUNT_TYPE_MAPPING } from '$constants/account/ACCOUNT_TYPE_MAPPING';
@@ -44,60 +44,56 @@ export const AccountList: FC<AccountListProps> = ({
         const Icon = ACCOUNT_TYPE_MAPPING[type].Icon;
 
         return (
-          <Link
-            id={id}
-            href={`/accounts/${id}`}
-            testId="account-row"
-            key={id}
-            className={clsx(
-              'bg-layer hover:bg-accent active:bg-accent',
-              'p-4 rounded-md',
-              'grid gap-4',
-            )}
-            transition="slideInFromRight"
-            hasHoverEffect={false}
-          >
-            <div className="grid grid-cols-[auto_1fr_auto] gap-4 items-center">
-              <Icon />
-              <Heading noMargin className="truncate" testId="account-name">
-                {name}
-              </Heading>
-              <span
-                className={clsx(
-                  'inline-flex items-center gap-2 text-sm shrink-0 relative text-muted-foreground',
-                  'after:w-3 after:h-3 after:rounded-full after:block',
-                  type === AccountType.Savings &&
-                    'after:bg-(--account-SAVINGS)',
-                  type === AccountType.Cash && 'after:bg-(--account-CASH)',
-                  type === AccountType.LongTermSavings &&
-                    'after:bg-(--account-LONG-TERM-SAVINGS)',
-                  type === AccountType.PreAssignedCash &&
-                    'after:bg-(--account-PRE-ASSIGNED-CASH)',
-                  type === AccountType.Investment &&
-                    'after:bg-(--account-INVESTMENT)',
-                  type === AccountType.Credit && 'after:bg-(--account-CREDIT)',
-                  type === AccountType.Loan && 'after:bg-(--account-LOAN)',
-                )}
-              >
-                {ACCOUNT_TYPE_MAPPING[type].label}
-              </span>
-            </div>
-            <div className="flex items-end justify-between gap-4">
-              <BalanceDisplay
-                className="[&_[data-slot='label']]:sr-only [&_[data-slot='balance']]:text-xl shrink-0"
-                label="Account Balance"
-                amount={currentDateBalance ?? balance}
-              />
-              {!!currentDateBalance && currentDateBalance !== balance && (
-                <p className="mt-0.5 text-sm text-muted-foreground truncate">
-                  <span className="sr-only">Upcoming Balance: </span>
-                  <span data-testid="upcoming-balance">
-                    {formatCurrency(balance)}
-                  </span>
-                </p>
+          <Fragment key={id}>
+            <style>{`
+              [data-account-item='${id}'] {
+                ${`--color-account: ${ACCOUNT_TYPE_MAPPING[type].color};`}
+              }
+            `}</style>
+            <Link
+              id={id}
+              href={`/accounts/${id}`}
+              testId="account-row"
+              className={clsx(
+                'bg-layer hover:bg-accent active:bg-accent',
+                'p-4 rounded-md',
+                'grid gap-4',
               )}
-            </div>
-          </Link>
+              transition="slideInFromRight"
+              hasHoverEffect={false}
+              data-account-item={id}
+            >
+              <div className="grid grid-cols-[auto_1fr_auto] gap-4 items-center">
+                <Icon />
+                <Heading noMargin className="truncate" testId="account-name">
+                  {name}
+                </Heading>
+                <span
+                  className={clsx(
+                    'inline-flex items-center gap-2 text-sm shrink-0 relative text-muted-foreground',
+                    'after:w-3 after:h-3 after:rounded-full after:block after:bg-(--color-account)',
+                  )}
+                >
+                  {ACCOUNT_TYPE_MAPPING[type].label}
+                </span>
+              </div>
+              <div className="flex items-end justify-between gap-4">
+                <BalanceDisplay
+                  className="[&_[data-slot='label']]:sr-only [&_[data-slot='balance']]:text-xl shrink-0"
+                  label="Account Balance"
+                  amount={currentDateBalance ?? balance}
+                />
+                {!!currentDateBalance && currentDateBalance !== balance && (
+                  <p className="mt-0.5 text-sm text-muted-foreground truncate">
+                    <span className="sr-only">Upcoming Balance: </span>
+                    <span data-testid="upcoming-balance">
+                      {formatCurrency(balance)}
+                    </span>
+                  </p>
+                )}
+              </div>
+            </Link>
+          </Fragment>
         );
       })}
     </List>
