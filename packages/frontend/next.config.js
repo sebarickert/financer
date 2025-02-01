8b753c422fab64db0601a6fd5810f72a4b9cff54
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2025-02-01 13:03:24 +0200

    Fix Drawer component transition issue in Chromium-based browsers

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -12,68 +12,74 @@ const nextConfig = {
   },
 };
 
-/**
- * @type {import('next-pwa').PWAConfig}
- */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: isDevelopment,
-  dynamicStartUrl: true,
-  sw: "/service-worker.js",
-  runtimeCaching: [
-    {
-      urlPattern: ({ url }) =>
-        url.pathname.startsWith("/api") || url.pathname.startsWith("/auth"),
-      handler: "NetworkOnly",
-    },
-  ],
-});
-
-module.exports = withPWA(nextConfig);
-
 // Injected content via Sentry wizard below
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+if (isDevelopment) {
+  // next-pwa has some webpack configs that throws warnings with turbopack.
+  // We don't want to enable sentry in the development mode.
+  module.exports = nextConfig;
+} else {
+  /**
+   * @type {import('next-pwa').PWAConfig}
+   */
+  const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: isDevelopment,
+    dynamicStartUrl: true,
+    sw: "/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) =>
+          url.pathname.startsWith("/api") || url.pathname.startsWith("/auth"),
+        handler: "NetworkOnly",
+      },
+    ],
+  });
 
-  org: "silte",
-  project: "frontend",
-  sentryUrl: "https://sentry.silte.fi/",
+  module.exports = withPWA(nextConfig);
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  module.exports = withSentryConfig(module.exports, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    org: "silte",
+    project: "frontend",
+    sentryUrl: "https://sentry.silte.fi/",
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+    // Automatically annotate React components to show their full name in breadcrumbs and session replay
+    reactComponentAnnotation: {
+      enabled: true,
+    },
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: "/monitoring",
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
-});
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: false,
+  });
+}
