2ae28884227dff570e3ba498072618879e872f56
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2025-02-02 20:29:00 +0200

    Fix backend sentry config

--- packages/backend/src/instrument.ts
+++ packages/backend/src/instrument.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
-  dsn: 'https://51a65ebdc1eaad371ce3246c706e36b4@sentry.silte.fi/2',
+  dsn: 'https://51a65ebdc1eaad371ce3246c706e36b4@sentry.silte.fi/5',
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
