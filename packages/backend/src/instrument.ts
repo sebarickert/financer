ca6151cad5ec2615076f2c495d5d30b08afd6d28
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2025-02-02 19:44:43 +0200

    Ci (#451)
    * Add support to opt-in for sentry
    
    * Update sentry projects
    
    * Update build configurations

--- packages/backend/src/instrument.ts
+++ packages/backend/src/instrument.ts
@@ -2,11 +2,13 @@ import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
-  dsn: 'https://426b216406159c3a2490b87ac97f57db@sentry.silte.fi/2',
+  dsn: 'https://51a65ebdc1eaad371ce3246c706e36b4@sentry.silte.fi/2',
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
   // Set sampling rate for profiling - this is relative to tracesSampleRate
   profilesSampleRate: 1.0,
+
+  enabled: process.env.SENTRY_ENABLED_BACKEND === 'true',
 });
--- packages/frontend/sentry.edge.config.ts
+++ packages/frontend/sentry.edge.config.ts
@@ -6,11 +6,13 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://34be80b93a3f1cf0b0d784b103adb47d@sentry.silte.fi/3',
+  dsn: 'https://6ea60a857dc59f06fd8c59a9a70fc79e@sentry.silte.fi/4',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  enabled: process.env.SENTRY_ENABLED_FRONTEND_SSR === 'production',
 });
