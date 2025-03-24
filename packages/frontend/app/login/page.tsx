348ca82579f4dd75a6a4ade36aadadf7fa44f97e
Sebastian Rickert <22739952+sebarickert@users.noreply.github.com>
2025-03-24 21:34:09 +0200

    Prop env variables down to login page (#459)

--- packages/frontend/app/login/page.tsx
+++ packages/frontend/app/login/page.tsx
@@ -1,12 +1,24 @@
 import { Metadata } from 'next';
-import { FC } from 'react';
 
-import { LoginContainer } from '@/container/LoginContainer';
+import { Login } from '@/views/Login';
 
 export const metadata: Metadata = {
   title: 'Login',
 };
 
-const LoginPage: FC = () => <LoginContainer />;
+const {
+  NEXT_PUBLIC_IS_GITHUB_OAUTH_ENABLED,
+  NEXT_PUBLIC_IS_AUTH0_OAUTH_ENABLED,
+} = process.env;
+
+const checkIsEnabled = (stringBoolean: string | undefined) =>
+  !!(stringBoolean && stringBoolean?.toLowerCase?.() === 'true');
+
+const LoginPage = () => (
+  <Login
+    github={checkIsEnabled(NEXT_PUBLIC_IS_GITHUB_OAUTH_ENABLED)}
+    auth0={checkIsEnabled(NEXT_PUBLIC_IS_AUTH0_OAUTH_ENABLED)}
+  />
+);
 
 export default LoginPage;
