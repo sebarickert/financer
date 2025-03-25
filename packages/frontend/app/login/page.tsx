258df42ce613f6b3969aee4008ca3d3684924517
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2025-03-25 12:03:24 +0200

    Refactor login OAuth environment variables for clarity and consistency

--- packages/frontend/app/login/page.tsx
+++ packages/frontend/app/login/page.tsx
@@ -6,18 +6,15 @@ export const metadata: Metadata = {
   title: 'Login',
 };
 
-const {
-  NEXT_PUBLIC_IS_GITHUB_OAUTH_ENABLED,
-  NEXT_PUBLIC_IS_AUTH0_OAUTH_ENABLED,
-} = process.env;
+const { LOGIN_IS_GITHUB_ENABLED, LOGIN_IS_AUTH_0_ENABLED } = process.env;
 
 const checkIsEnabled = (stringBoolean: string | undefined) =>
   !!(stringBoolean && stringBoolean?.toLowerCase?.() === 'true');
 
 const LoginPage = () => (
   <Login
-    github={checkIsEnabled(NEXT_PUBLIC_IS_GITHUB_OAUTH_ENABLED)}
-    auth0={checkIsEnabled(NEXT_PUBLIC_IS_AUTH0_OAUTH_ENABLED)}
+    github={checkIsEnabled(LOGIN_IS_GITHUB_ENABLED)}
+    auth0={checkIsEnabled(LOGIN_IS_AUTH_0_ENABLED)}
   />
 );
 
