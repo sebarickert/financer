952b46f857c707198f1bd619d549d537a022e80c
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2025-06-14 22:48:26 +0300

    Add Playwright environment variable and improve Drawer component styles

--- packages/frontend/src/components/blocks/Drawer.tsx
+++ packages/frontend/src/components/blocks/Drawer.tsx
@@ -62,7 +62,7 @@ export const Drawer: FC<DrawerProps> = ({
     // Open styles
     'bg-background text-foreground fixed text-left',
     'max-lg:bottom-0 max-lg:inset-x-0 max-lg:top-auto max-lg:w-full max-lg:rounded-t-[10px] max-lg:border max-lg:group-open:translate-y-0',
-    'lg:max-w-[600px] lg:w-full lg:left-auto lg:h-full lg:group-open:translate-x-0 lg:border-l',
+    'lg:max-w-[600px] lg:w-full lg:left-auto lg:right-0 lg:h-full lg:group-open:translate-x-0 lg:border-l',
     'pt-0 pb-safe-offset-12 px-6 lg:px-8',
     'max-h-dvh',
     className,
@@ -88,14 +88,14 @@ export const Drawer: FC<DrawerProps> = ({
       />
 
       <div
-        className={clsx('z-0', drawerContentBaseClasses)}
+        className={clsx('overflow-auto', drawerContentBaseClasses)}
         data-testid="drawer-content"
       >
         <div
           className={clsx(
             'flex justify-between items-center',
             'bg-background',
-            'sticky top-0',
+            'sticky top-0 z-10',
             'pt-2 pb-2 mb-8',
             'px-6 lg:px-8 -mx-6 lg:-mx-8',
             'border-b',
@@ -114,8 +114,10 @@ export const Drawer: FC<DrawerProps> = ({
             <span className="sr-only">Close Drawer</span>
           </Button>
         </div>
-        {description && <p className="mb-8">{description}</p>}
-        {children}
+        <div className="isolate">
+          {description && <p className="mb-8">{description}</p>}
+          {children}
+        </div>
       </div>
     </dialog>
   );
